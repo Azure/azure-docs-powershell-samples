54dba6f9db10ea3b2cfbaba83d3b3aebaebae7f6
Larry Franks <larry.franks@microsoft.com>
2017-03-27 15:51:59 -0400

    hacking around

---
+++ hdinsight/use-virtual-network-with-hdinsight/use-virtual-network-with-hdinsight.ps1
@@ -0,0 +1,141 @@
+function New-HDInsightWithVNet {
+    # Script should stop on failures
+    $ErrorActionPreference = "Stop"
+
+    # Login to your Azure subscription
+    $sub = Get-AzureRmSubscription -ErrorAction SilentlyContinue
+    if(-not($sub))
+    {
+        Add-AzureRmAccount
+    }
+
+    # If you have multiple subscriptions, uncomment the following
+    # and use it to set the subscription used in this script
+    # Select-AzureRmSubscription -SubscriptionName "Name of subscription"
+
+    # Prompt for the base required information
+    $baseName = Read-Host -Prompt "Enter the base name for Azure resources created by this script"
+    $resourceGroupName = Read-Host -Prompt "Enter the Azure Resource Group name"
+    $location = Read-Host -Prompt "Enter the Azure region to use"
+    $rootCert = Read-Host -Prompt "Enter the file path to the VPN root certificate"
+    # Prompt for HDInsight credentials
+    $adminCreds = Get-Credential -Message "Enter the HTTPS user name and password for the HDInsight cluster" -UserName "admin"
+    $sshCreds = Get-Credential -Message "Enter the SSH user name and password for the HDInsight cluster" -UserName "sshuser"
+    # Get the path to the VPN certificate
+
+    # Names for Azure resources
+    $networkName = "net-$baseName"
+    $clusterName = "hdi-$baseName"
+    $storageName = "store$baseName" # Can't use dashes in storage names
+    $defaultContainerName = $clusterName
+    $defaultSubnetName = "default"
+    $gatewaySubnetName = "GatewaySubnet"
+    $gatewayPublicIpName = "GatewayIp"
+    $gatewayIpConfigName = "GatewayConfig"
+    $vpnRootCertName = "rootcert"
+    $vpnName = "VPNGateway"
+
+    # Network settings
+    $networkAddressPrefix = "10.0.0.0/16"
+    $defaultSubnetPrefix = "10.0.0.0/24"
+    $gatewaySubnetPrefix = "10.0.1.0/24"
+    $vpnClientAddressPool = "172.16.201.0/24"
+
+    # Get the certificate info
+    # Get the full path in case a relative path was passed
+    $rootCertFile = Get-ChildItem $rootCert
+    $cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2($rootCertFile)
+    $certBase64 = [System.Convert]::ToBase64String($cert.RawData)
+    $p2sRootCert = New-AzureRmVpnClientRootCertificate -Name $vpnRootCertName `
+        -PublicCertData $certBase64
+
+    # HDInsight settings
+    $HdiWorkerNodes = 4
+    $hdiVersion = "3.5"
+    $hdiType = "Kafka"
+
+    # Create the resource group that will contain everything
+    New-AzureRmResourceGroup -Name $resourceGroupName -Location $location
+
+    # Create the subnet configuration
+    $defaultSubnetConfig = New-AzureRmVirtualNetworkSubnetConfig -Name $defaultSubnetName `
+        -AddressPrefix $defaultSubnetPrefix
+    $gatewaySubnetConfig = New-AzureRmVirtualNetworkSubnetConfig -Name $gatewaySubnetName `
+        -AddressPrefix $gatewaySubnetPrefix
+
+    # Create the subnet
+    New-AzureRmVirtualNetwork -Name $networkName `
+        -ResourceGroupName $resourceGroupName `
+        -Location $location `
+        -AddressPrefix $networkAddressPrefix `
+        -Subnet $defaultSubnetConfig, $gatewaySubnetConfig
+
+    # Get the network & subnet that were created
+    $network = Get-AzureRmVirtualNetwork -Name $networkName `
+        -ResourceGroupName $resourceGroupName
+    $gatewaySubnet = Get-AzureRmVirtualNetworkSubnetConfig -Name $gatewaySubnetName `
+        -VirtualNetwork $network
+    $defaultSubnet = Get-AzureRmVirtualNetworkSubnetConfig -Name $defaultSubnetName `
+        -VirtualNetwork $network
+
+    # Set a dynamic public IP address for the gateway subnet
+    $gatewayPublicIp = New-AzureRmPublicIpAddress -Name $gatewayPublicIpName `
+        -ResourceGroupName $resourceGroupName `
+        -Location $location `
+        -AllocationMethod Dynamic
+
+    $gatewayIpConfig = New-AzureRmVirtualNetworkGatewayIpConfig -Name $gatewayIpConfigName `
+        -Subnet $gatewaySubnet `
+        -PublicIpAddress $gatewayPublicIp
+
+    # Create the VPN gateway
+    New-AzureRmVirtualNetworkGateway -Name $vpnName `
+        -ResourceGroupName $resourceGroupName `
+        -Location $location `
+        -IpConfigurations $gatewayIpConfig `
+        -GatewayType Vpn `
+        -VpnType RouteBased `
+        -EnableBgp $false `
+        -GatewaySku Standard `
+        -VpnClientAddressPool $vpnClientAddressPool `
+        -VpnClientRootCertificates $p2sRootCert
+
+    # Create the storage account
+    New-AzureRmStorageAccount `
+        -ResourceGroupName $resourceGroupName `
+        -Name $storageName `
+        -Type Standard_GRS `
+        -Location $location
+
+    # Get the storage account keys and create a context
+    $defaultStorageKey = (Get-AzureRmStorageAccountKey -ResourceGroupName $resourceGroupName `
+        -Name $storageName)[0].Value
+    $storageContext = New-AzureStorageContext -StorageAccountName $storageName `
+        -StorageAccountKey $defaultStorageKey
+
+    # Create the default storage container
+    New-AzureStorageContainer -Name $defaultContainerName `
+        -Context $storageContext
+
+    # Create the HDInsight cluster
+    New-AzureRmHDInsightCluster `
+        -ResourceGroupName $resourceGroupName `
+        -ClusterName $clusterName `
+        -Location $location `
+        -ClusterSizeInNodes $hdiWorkerNodes `
+        -ClusterType $hdiType `
+        -OSType Linux `
+        -Version $hdiVersion `
+        -HttpCredential $adminCreds `
+        -SshCredential $sshCreds `
+        -DefaultStorageAccountName "$storageName.blob.core.windows.net" `
+        -DefaultStorageAccountKey $defaultStorageKey `
+        -DefaultStorageContainer $defaultContainerName `
+        -VirtualNetworkId $network.Id `
+        -SubnetName $defaultSubnet.Id
+
+    # Get the URI for the the Windows VPN client installer download
+    Get-AzureRmVpnClientPackage -ResourceGroupName $resourceGroupName `
+        -VirtualNetworkGatewayName $vpnName `
+        -ProcessorArchitecture Amd64
+}
