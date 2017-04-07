929d5a285c5912620eda0cbfff4b9b7f20016882
Larry Franks <larry.franks@microsoft.com>
2017-04-07 15:46:01 -0400

    cluster creation script and updates to vnet

--- hdinsight/use-virtual-network-with-hdinsight/use-virtual-network-with-hdinsight.ps1
+++ hdinsight/use-virtual-network-with-hdinsight/use-virtual-network-with-hdinsight.ps1
@@ -1,4 +1,4 @@
-function New-HDInsightWithVNet {
+function New-HDInsightAndVNet {
     # Script should stop on failures
     $ErrorActionPreference = "Stop"
 
@@ -13,50 +13,110 @@ function New-HDInsightWithVNet {
     # and use it to set the subscription used in this script
     # Select-AzureRmSubscription -SubscriptionName "Name of subscription"
 
-    # Prompt for the base required information
-    $baseName = Read-Host -Prompt "Enter the base name for Azure resources created by this script"
+    # Prompt for the information
     $resourceGroupName = Read-Host -Prompt "Enter the Azure Resource Group name"
     $location = Read-Host -Prompt "Enter the Azure region to use"
-    $rootCert = Read-Host -Prompt "Enter the file path to the VPN root certificate"
+    $networkName = Read-Host -Prompt "Enter the name of the virtual network"
+    $storageName = Read-Host -Prompt "Enter the storage account name"
+    $clusterName = Read-Host -Prompt "Enter the HDInsight cluster name"
     # Prompt for HDInsight credentials
     $adminCreds = Get-Credential -Message "Enter the HTTPS user name and password for the HDInsight cluster" -UserName "admin"
     $sshCreds = Get-Credential -Message "Enter the SSH user name and password for the HDInsight cluster" -UserName "sshuser"
-    # Get the path to the VPN certificate
-
-    # Names for Azure resources
-    $networkName = "net-$baseName"
-    $clusterName = "hdi-$baseName"
-    $storageName = "store$baseName" # Can't use dashes in storage names
+    
+    # Set some values
     $defaultContainerName = $clusterName
+    $hdiWorkerNodes = 4
+    $hdiVersion = "3.5"
+    $hdiType = "Hadoop"
+    
+    # Network settings
     $defaultSubnetName = "default"
+    $networkAddressPrefix = "10.0.0.0/16"
+    $defaultSubnetPrefix = "10.0.0.0/24"
+
+    # Create the resource group that will contain everything
+    New-AzureRmResourceGroup -Name $resourceGroupName -Location $location
+
+    # Create the subnet configuration
+    $defaultSubnetConfig = New-AzureRmVirtualNetworkSubnetConfig -Name $defaultSubnetName `
+        -AddressPrefix $defaultSubnetPrefix
+
+    # Create the subnet
+    New-AzureRmVirtualNetwork -Name $networkName `
+        -ResourceGroupName $resourceGroupName `
+        -Location $location `
+        -AddressPrefix $networkAddressPrefix `
+        -Subnet $defaultSubnetConfig
+
+    # Get the network & subnet that were created
+    $network = Get-AzureRmVirtualNetwork -Name $networkName `
+        -ResourceGroupName $resourceGroupName
+
+    $defaultSubnet = Get-AzureRmVirtualNetworkSubnetConfig -Name $defaultSubnetName `
+        -VirtualNetwork $network
+
+    # Create the storage account
+    New-AzureRmStorageAccount `
+        -ResourceGroupName $resourceGroupName `
+        -Name $storageName `
+        -Type Standard_GRS `
+        -Location $location
+
+    # Get the storage account keys and create a context
+    $defaultStorageKey = (Get-AzureRmStorageAccountKey -ResourceGroupName $resourceGroupName `
+        -Name $storageName)[0].Value
+    $storageContext = New-AzureStorageContext -StorageAccountName $storageName `
+        -StorageAccountKey $defaultStorageKey
+
+    # Create the default storage container
+    New-AzureStorageContainer -Name $defaultContainerName `
+        -Context $storageContext
+
+    # Create the HDInsight cluster
+    New-AzureRmHDInsightCluster `
+        -ResourceGroupName $resourceGroupName `
+        -ClusterName $clusterName `
+        -Location $location `
+        -ClusterSizeInNodes $hdiWorkerNodes `
+        -ClusterType $hdiType `
+        -OSType Linux `
+        -Version $hdiVersion `
+        -HttpCredential $adminCreds `
+        -SshCredential $sshCreds `
+        -DefaultStorageAccountName "$storageName.blob.core.windows.net" `
+        -DefaultStorageAccountKey $defaultStorageKey `
+        -DefaultStorageContainer $defaultContainerName `
+        -VirtualNetworkId $network.Id `
+        -SubnetName $defaultSubnet.Id
+}
+
+function New-VPNGateway {
+    # Prompt for the information
+    $resourceGroupName = Read-Host -Prompt "Enter the Azure Resource Group name"
+    $location = Read-Host -Prompt "Enter the Azure region to use"
+    $networkName = Read-Host -Prompt "Enter the name of the virtual network"
+    $rootCert = Read-Host -Prompt "Enter the path to the VPN root certificate"
+
+    # Network settings
+    $defaultSubnetName = "default"
+    $networkAddressPrefix = "10.0.0.0/16"
+    $gatewaySubnetPrefix = "10.0.1.0/24"
+    $vpnClientAddressPool = "172.16.201.0/24"
     $gatewaySubnetName = "GatewaySubnet"
     $gatewayPublicIpName = "GatewayIp"
     $gatewayIpConfigName = "GatewayConfig"
     $vpnRootCertName = "rootcert"
     $vpnName = "VPNGateway"
 
-    # Network settings
-    $networkAddressPrefix = "10.0.0.0/16"
-    $defaultSubnetPrefix = "10.0.0.0/24"
-    $gatewaySubnetPrefix = "10.0.1.0/24"
-    $vpnClientAddressPool = "172.16.201.0/24"
-
     # Get the certificate info
     # Get the full path in case a relative path was passed
     $rootCertFile = Get-ChildItem $rootCert
     $cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2($rootCertFile)
     $certBase64 = [System.Convert]::ToBase64String($cert.RawData)
-    $p2sRootCert = New-AzureRmVpnClientRootCertificate -Name $vpnRootCertName `
+    $p2sRootCert = New-AzureRmVpnClientRootCertificate `
+        -Name $vpnRootCertName `
         -PublicCertData $certBase64
-
-    # HDInsight settings
-    $HdiWorkerNodes = 4
-    $hdiVersion = "3.5"
-    $hdiType = "Kafka"
-
-    # Create the resource group that will contain everything
-    New-AzureRmResourceGroup -Name $resourceGroupName -Location $location
-
+    
     # Create the subnet configuration
     $defaultSubnetConfig = New-AzureRmVirtualNetworkSubnetConfig -Name $defaultSubnetName `
         -AddressPrefix $defaultSubnetPrefix
@@ -99,40 +159,6 @@ function New-HDInsightWithVNet {
         -GatewaySku Standard `
         -VpnClientAddressPool $vpnClientAddressPool `
         -VpnClientRootCertificates $p2sRootCert
-
-    # Create the storage account
-    New-AzureRmStorageAccount `
-        -ResourceGroupName $resourceGroupName `
-        -Name $storageName `
-        -Type Standard_GRS `
-        -Location $location
-
-    # Get the storage account keys and create a context
-    $defaultStorageKey = (Get-AzureRmStorageAccountKey -ResourceGroupName $resourceGroupName `
-        -Name $storageName)[0].Value
-    $storageContext = New-AzureStorageContext -StorageAccountName $storageName `
-        -StorageAccountKey $defaultStorageKey
-
-    # Create the default storage container
-    New-AzureStorageContainer -Name $defaultContainerName `
-        -Context $storageContext
-
-    # Create the HDInsight cluster
-    New-AzureRmHDInsightCluster `
-        -ResourceGroupName $resourceGroupName `
-        -ClusterName $clusterName `
-        -Location $location `
-        -ClusterSizeInNodes $hdiWorkerNodes `
-        -ClusterType $hdiType `
-        -OSType Linux `
-        -Version $hdiVersion `
-        -HttpCredential $adminCreds `
-        -SshCredential $sshCreds `
-        -DefaultStorageAccountName "$storageName.blob.core.windows.net" `
-        -DefaultStorageAccountKey $defaultStorageKey `
-        -DefaultStorageContainer $defaultContainerName `
-        -VirtualNetworkId $network.Id `
-        -SubnetName $defaultSubnet.Id
 
     # Get the URI for the the Windows VPN client installer download
     Get-AzureRmVpnClientPackage -ResourceGroupName $resourceGroupName `
