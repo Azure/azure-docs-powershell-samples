fbc591429b1b20aace8b064228dbf1fc677d2b0e
George Wallace <gwallace@Microsoft.com>
2017-05-18 22:46:51 -0700

    updates to syntax

--- virtual-network/filter-network-traffic/filter-network-traffic.ps1
+++ virtual-network/filter-network-traffic/filter-network-traffic.ps1
@@ -3,48 +3,48 @@ $rgName='MyResourceGroup'
 $location='eastus'
 
 # Create user object
-$cred = Get-Credential -Message "Enter a username and password for the virtual machine."
+$cred = Get-Credential -Message 'Enter a username and password for the virtual machine.'
 
 # Create a resource group.
 New-AzureRmResourceGroup -Name $rgName -Location $location
 
 # Create a virtual network, a front-end subnet, and a back-end subnet.
-$fesubnet = New-AzureRmVirtualNetworkSubnetConfig -Name 'MySubnet-FrontEnd' -AddressPrefix 10.0.1.0/24
-$besubnet = New-AzureRmVirtualNetworkSubnetConfig -Name 'MySubnet-BackEnd' -AddressPrefix 10.0.2.0/24
+$fesubnet = New-AzureRmVirtualNetworkSubnetConfig -Name 'MySubnet-FrontEnd' -AddressPrefix '10.0.1.0/24'
+$besubnet = New-AzureRmVirtualNetworkSubnetConfig -Name 'MySubnet-BackEnd' -AddressPrefix '10.0.2.0/24'
 
-$vnet = New-AzureRmVirtualNetwork -ResourceGroupName $rgName -Name MyVnet -AddressPrefix 10.0.0.0/16 `
+$vnet = New-AzureRmVirtualNetwork -ResourceGroupName $rgName -Name 'MyVnet' -AddressPrefix '10.0.0.0/16' `
   -Location $location -Subnet $fesubnet, $besubnet
 
 # Create NSG rules to allow HTTP & HTTPS traffic inbound.
-$rule1 = New-AzureRmNetworkSecurityRuleConfig -Name Allow-HTTP-ALL -Description "Allow HTTP" `
+$rule1 = New-AzureRmNetworkSecurityRuleConfig -Name 'Allow-HTTP-ALL' -Description 'Allow HTTP' `
   -Access Allow -Protocol Tcp -Direction Inbound -Priority 100 `
   -SourceAddressPrefix Internet -SourcePortRange * `
   -DestinationAddressPrefix * -DestinationPortRange 80
 
-$rule2 = New-AzureRmNetworkSecurityRuleConfig -Name Allow-HTTPS-All -Description "Allow HTTPS" `
+$rule2 = New-AzureRmNetworkSecurityRuleConfig -Name 'Allow-HTTPS-All' -Description 'Allow HTTPS' `
   -Access Allow -Protocol Tcp -Direction Inbound -Priority 200 `
   -SourceAddressPrefix Internet -SourcePortRange * `
-  -DestinationAddressPrefix * -DestinationPortRange 80
+  -DestinationAddressPrefix * -DestinationPortRange 443
 
-# Create an NSG rule to allow SSH traffic in from the Internet to the front-end subnet.
-$rule3 = New-AzureRmNetworkSecurityRuleConfig -Name Allow-SSH-All -Description "Allow SSH" `
+# Create an NSG rule to allow RDP traffic in from the Internet to the front-end subnet.
+$rule3 = New-AzureRmNetworkSecurityRuleConfig -Name 'Allow-RDP-All' -Description 'Allow RDP' `
   -Access Allow -Protocol Tcp -Direction Inbound -Priority 300 `
   -SourceAddressPrefix Internet -SourcePortRange * `
-  -DestinationAddressPrefix * -DestinationPortRange 22
+  -DestinationAddressPrefix * -DestinationPortRange 3389
 
 # Create a network security group (NSG) for the front-end subnet.
 $nsg = New-AzureRmNetworkSecurityGroup -ResourceGroupName $RgName -Location $location `
   -Name "MyNsg-FrontEnd" -SecurityRules $rule1,$rule2,$rule3
 
 # Associate the front-end NSG to the front-end subnet.
-Set-AzureRmVirtualNetworkSubnetConfig -VirtualNetwork $vnet -Name MySubnet-FrontEnd `
+Set-AzureRmVirtualNetworkSubnetConfig -VirtualNetwork $vnet -Name 'MySubnet-FrontEnd' `
   -AddressPrefix 10.0.1.0/24 -NetworkSecurityGroup $nsg
 
 # Create an NSG rule to block all outbound traffic from the back-end subnet to the Internet (inbound blocked by default).
 
-$rule1 = New-AzureRmNetworkSecurityRuleConfig -Name Deny-Internet-All -Description "Deny all Internet" `
-  -Access Allow -Protocol Tcp -Direction Inbound -Priority 100 `
-  -SourceAddressPrefix Internet -SourcePortRange * `
+$rule1 = New-AzureRmNetworkSecurityRuleConfig -Name 'Deny-Internet-All' -Description "Deny all Internet" `
+  -Access Allow -Protocol Tcp -Direction Outbound -Priority 100 `
+  -SourceAddressPrefix * -SourcePortRange * `
   -DestinationAddressPrefix * -DestinationPortRange *
 
 # Create a network security group for the back-end subnet.
@@ -68,10 +68,10 @@ $nicVMbe = New-AzureRmNetworkInterface -ResourceGroupName $rgName -Location $loc
   -Name MyNic-BackEnd -Subnet $vnet.Subnets[1]
 
 # Create the VM with both the FrontEnd and BackEnd NICs.
-$vmConfig = New-AzureRmVMConfig -VMName myVM2 -VMSize Standard_DS2 | `
-  Set-AzureRmVMOperatingSystem -Windows -ComputerName myVM2 -Credential $cred | `
-  Set-AzureRmVMSourceImage -PublisherName MicrosoftWindowsServer -Offer WindowsServer `
-  -Skus 2016-Datacenter -Version latest
+$vmConfig = New-AzureRmVMConfig -VMName 'myVM2' -VMSize Standard_DS2 | `
+  Set-AzureRmVMOperatingSystem -Windows -ComputerName 'myVM2' -Credential $cred | `
+  Set-AzureRmVMSourceImage -PublisherName 'MicrosoftWindowsServer' -Offer 'WindowsServer' `
+  -Skus '2016-Datacenter' -Version 'latest'
     
 $vmconfig = Add-AzureRmVMNetworkInterface -VM $vmConfig -id $nicVMfe.Id -Primary
 $vmconfig = Add-AzureRmVMNetworkInterface -VM $vmConfig -id $nicVMbe.Id
--- virtual-network/peer-two-virtual-networks/peer-two-virtual-networks.ps1
+++ virtual-network/peer-two-virtual-networks/peer-two-virtual-networks.ps1
@@ -6,13 +6,13 @@ $location='eastus'
 New-AzureRmResourceGroup -Name $rgName -Location $location
 
 # Create virtual network 1.
-$vnet1 = New-AzureRmVirtualNetwork -ResourceGroupName $rgName -Name Vnet1 -AddressPrefix 10.0.0.0/16 -Location $location
+$vnet1 = New-AzureRmVirtualNetwork -ResourceGroupName $rgName -Name 'Vnet1' -AddressPrefix '10.0.0.0/16' -Location $location
 
 # Create virtual network 2.
-$vnet2 = New-AzureRmVirtualNetwork -ResourceGroupName $rgName -Name Vnet2 -AddressPrefix 10.1.0.0/16 -Location $location
+$vnet2 = New-AzureRmVirtualNetwork -ResourceGroupName $rgName -Name 'Vnet2' -AddressPrefix '10.1.0.0/16' -Location $location
 
 # Peer VNet1 to VNet2.
-Add-AzureRmVirtualNetworkPeering -Name LinkVnet1ToVnet2 -VirtualNetwork $vnet1 -RemoteVirtualNetworkId $vnet2.Id
+Add-AzureRmVirtualNetworkPeering -Name 'LinkVnet1ToVnet2' -VirtualNetwork $vnet1 -RemoteVirtualNetworkId $vnet2.Id
 
 # Peer VNet2 to VNet1.
-Add-AzureRmVirtualNetworkPeering -Name LinkVnet2ToVnet1 -VirtualNetwork $vnet2 -RemoteVirtualNetworkId $vnet1.Id
+Add-AzureRmVirtualNetworkPeering -Name 'LinkVnet2ToVnet1' -VirtualNetwork $vnet2 -RemoteVirtualNetworkId $vnet1.Id
