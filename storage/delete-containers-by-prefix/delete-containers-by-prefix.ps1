bba9fd0be50afdf7728b8365d1cbb30b0460be95
Marsh Macy <marsma@microsoft.com>
2017-06-12 14:39:55 -0700

    add delete containers by prefix back in

--- storage/delete-containers-by-prefix/delete-containers-by-prefix.ps1
+++ storage/delete-containers-by-prefix/delete-containers-by-prefix.ps1
@@ -1,19 +1,21 @@
 ﻿# this script will show how to delete containers with a specific prefix 
-# the prefix this will search for is "Image". 
-# before running this, you need to create a storage account, create a container,
-#    and upload some blobs into the container 
-# note: this retrieves all of the matching blobs in one command 
-#       if you are going to run this against a container with a lot of blobs
+# the prefix this will search for is "image". 
+# before running this, you need to create a storage account, create some containers,
+#    some having the same prefix so you can test this
+# note: this retrieves all of the matching containers in one command 
+#       if you are going to run this against a storage account with a lot of containers
 #       (more than a couple hundred), use continuation tokens to retrieve
-#       the list of blobs. We will be adding a sample showing that scenario in the future.
+#       the list of containers. We will be adding a sample showing that scenario in the future.
+
+# login to your Azure account
+Login-AzureRmAccount
 
 # these are for the storage account to be used
 #   and the prefix for which to search
-$resourceGroup = "bloblisttestrg"
-$location = "westus"
-$storageAccountName = "contosobloblisttest"
-$containerName = "listtestblobs"
-$prefix = "Image"
+$resourceGroup = "containerdeletetestrg"
+$location = "eastus"
+$storageAccountName = "contosocontainerdeletetest"
+$prefix = "image"
 
 # get a reference to the storage account and the context
 $storageAccount = Get-AzureRmStorageAccount `
@@ -21,21 +23,21 @@ $storageAccount = Get-AzureRmStorageAccount `
   -Name $storageAccountName
 $ctx = $storageAccount.Context 
 
-# list all blobs in the container
-Write-Host "All blobs"
-Get-AzureStorageBlob -Container $ContainerName -Context $ctx | select Name
+# list all containers in the storage account 
+Write-Host "All containers"
+Get-AzureStorageContainer -Context $ctx | select Name
 
-# retrieve list of blobs to delete
-$listOfBlobsToDelete = Get-AzureStorageBlob -Container $ContainerName -Context $ctx -Prefix $prefix 
+# retrieve list of containers to delete
+$listOfContainersToDelete = Get-AzureStorageContainer -Context $ctx -Prefix $prefix
 
-# write list of blobs to be deleted 
-Write-Host "Blobs to be deleted"
-$listOfBlobsToDelete | select Name, Length
+# write list of containers to be deleted 
+Write-Host "Containers to be deleted"
+$listOfContainersToDelete | select Name
 
-# delete the blobs
-Write-Host "Deleting blobs"
-$listOfBlobsToDelete | ForEach-Object { Remove-AzureStorageBlob -Container $containerName -Context $ctx -Blob $_.Name}
+# delete the containers
+Write-Host "Deleting containers"
+$listOfContainersToDelete | ForEach-Object { Remove-AzureStorageContainer -Container $_.Name -Context $ctx }
 
-# show list of blobs not deleted 
-Write-Host "All Blobs not deleted"
-Get-AzureStorageBlob -Container $containerName -Context $ctx | select Name
+# show list of containers not deleted 
+Write-Host "All containers not deleted"
+Get-AzureStorageContainer -Context $ctx | select Name
