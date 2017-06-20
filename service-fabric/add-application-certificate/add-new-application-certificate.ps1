25e82df45be7b1ddb580d2043a4349604de30cad
Ryan Wike <ryanwi@microsoft.com>
2017-06-20 16:45:01 -0700

    adding script

---
+++ service-fabric/add-application-certificate/add-new-application-certificate.ps1
@@ -0,0 +1,16 @@
+﻿
+# Variables for common values.
+$clusterloc="SouthCentralUS"
+$groupname="mysfclustergroup"
+$clustername = "mysfcluster"
+$vaultname = "mykeyvault"
+$subname="$clustername.$clusterloc.cloudapp.azure.com"
+
+# Certificate variables.
+$appcertpwd = ConvertTo-SecureString -String 'Password#1234' -AsPlainText -Force
+$appcertfolder="c:\myappcertificates\"
+
+# Create a new self-signed certificate and add it to all the VMs in the cluster.
+Add-AzureRmServiceFabricApplicationCertificate -ResourceGroupName $groupname -Name $clustername `
+    -KeyVaultName $vaultname `-KeyVaultResouceGroupName $groupname -CertificateSubjectName $subname 
+    -CertificateOutputFolder $appcertfolder -CertificatePassword $appcertpwd
