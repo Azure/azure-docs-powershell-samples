8e7ac87ec02e6e3e9241c051c1573eac559de3b2
Ryan Wike <ryanwi@microsoft.com>
2017-06-20 18:15:24 -0700

    updated script

--- service-fabric/deploy-application/deploy-application.ps1
+++ service-fabric/deploy-application/deploy-application.ps1
@@ -1,1 +1,20 @@
-﻿
+﻿# Variables
+$endpoint = 'mysftestcluster.southcentralus.cloudapp.azure.com:19000'
+$thumbprint = '2779F0BB9A969FB88E04915FFE7955D0389DA7AF'
+$packagepath="C:\Users\sfuser\Documents\Visual Studio 2017\Projects\MyApplication\MyApplication\pkg\Release"
+
+# Connect to the cluster using a client certificate.
+Connect-ServiceFabricCluster -ConnectionEndpoint $endpoint `
+          -KeepAliveIntervalInSec 10 `
+          -X509Credential -ServerCertThumbprint $thumbprint `
+          -FindType FindByThumbprint -FindValue $thumbprint `
+          -StoreLocation CurrentUser -StoreName My
+
+# Copy the application package to the cluster image store.
+Copy-ServiceFabricApplicationPackage $packagepath -ImageStoreConnectionString fabric:ImageStore -ApplicationPackagePathInImageStore MyApplication
+
+# Register the application type.
+Register-ServiceFabricApplicationType MyApplication
+
+# Create the application instance.
+New-ServiceFabricApplication fabric:/MyApplication MyApplicationType 1.0.0
