895715eee6f4d6621b7efc875520516dc8474128
Ryan Wike <ryanwi@microsoft.com>
2017-08-16 17:02:50 -0700

    initial checkin

---
+++ service-fabric/upgrade-application/upgrade-application.ps1
@@ -0,0 +1,91 @@
+﻿Connect-ServiceFabricCluster -ConnectionEndpoint mysftestcluster.westus.cloudapp.azure.com:19000
+
+
+$ApplicationPackagePath = "C:\Users\sfuser\documents\visual studio 2017\Projects\Voting\Voting\pkg\Debug"
+# $ApplicationParameterFilePath = "C:\Users\sfuser\documents\visual studio 2017\Projects\Voting\Voting\ApplicationParameters\Cloud.xml"
+$ApplicationName = "fabric:/Voting"
+$ApplicationTypeName = "VotingType"
+$ApplicationTypeVersion = 1.0.0
+$imageStoreConnectionString = "fabric:ImageStore"
+$CopyPackageTimeoutSec = 600
+$CompressPackage = $true
+
+
+## Check existence of the application
+$oldApplication = Get-ServiceFabricApplication -ApplicationName $ApplicationName
+        
+if (!$oldApplication)
+{
+    $errMsg = "Application '$ApplicationName' doesn't exist in cluster."
+    throw $errMsg
+}
+else
+{
+    ## Check upgrade status
+    $upgradeStatus = Get-ServiceFabricApplicationUpgrade -ApplicationName $ApplicationName
+    if ($upgradeStatus.UpgradeState -ne "RollingBackCompleted" -and $upgradeStatus.UpgradeState -ne "RollingForwardCompleted" -and $upgradeStatus.UpgradeState -ne "Failed")
+    {
+        $errMsg = "An upgrade for the application '$names.ApplicationTypeName' is already in progress."
+        throw $errMsg
+    }
+
+    $reg = Get-ServiceFabricApplicationType -ApplicationTypeName $ApplicationTypeName | Where-Object  { $_.ApplicationTypeVersion -eq $ApplicationTypeVersion }
+    if ($reg)
+    {
+        Write-Host 'Application Type '$ApplicationTypeName' and Version '$ApplicationTypeVersion' was already registered with Cluster, unregistering it...'
+        $reg | Unregister-ServiceFabricApplicationType -Force
+    }
+
+    ## Copy application package to image store
+    $applicationPackagePathInImageStore = $ApplicationTypeName
+    Write-Host "Copying application package to image store..."
+    Copy-ServiceFabricApplicationPackage -ApplicationPackagePath $ApplicationPackagePath -ImageStoreConnectionString $imageStoreConnectionString -ApplicationPackagePathInImageStore $applicationPackagePathInImageStore -TimeOutSec $CopyPackageTimeoutSec -CompressPackage:$CompressPackage
+    if(!$?)
+    {
+        throw "Copying of application package to image store failed. Cannot continue with registering the application."
+    }
+    
+    ## Register application type
+    Write-Host "Registering application type..."
+    Register-ServiceFabricApplicationType -ApplicationPathInImageStore $applicationPackagePathInImageStore
+    if(!$?)
+    {
+        throw "Registration of application type failed."
+    }
+
+    $UpgradeParameters["ApplicationName"] = $ApplicationName
+    $UpgradeParameters["ApplicationTypeVersion"] = $ApplicationTypeVersion
+
+    ## Start monitored application upgrade
+    try
+    {
+        Write-Host "Start upgrading application..." 
+        Start-ServiceFabricApplicationUpgrade -ApplicationName $ApplicationName -ApplicationTypeVersion $ApplicationTypeVersion -HealthCheckStableDurationSec 60 -UpgradeDomainTimeoutSec 1200 -UpgradeTimeout 3000 -FailureAction Rollback -Monitored
+    }
+    catch
+    {
+        Write-Host "Unregister application type '$ApplicationTypeName' and version '$ApplicationTypeVersion' ..."
+        Unregister-ServiceFabricApplicationType -ApplicationTypeName $ApplicationTypeName -ApplicationTypeVersion $ApplicationTypeVersion -Force
+        throw
+    }
+
+    do
+    {
+        Write-Host "Waiting for upgrade..."
+        Start-Sleep -Seconds 3
+        $upgradeStatus = Get-ServiceFabricApplicationUpgrade -ApplicationName $ApplicationName
+    } while ($upgradeStatus.UpgradeState -ne "RollingBackCompleted" -and $upgradeStatus.UpgradeState -ne "RollingForwardCompleted" -and $upgradeStatus.UpgradeState -ne "Failed")
+    
+    if($upgradeStatus.UpgradeState -eq "RollingForwardCompleted")
+    {
+        Write-Host "Upgrade completed successfully."
+    }
+    elseif($upgradeStatus.UpgradeState -eq "RollingBackCompleted")
+    {
+        Write-Error "Upgrade was Rolled back."
+    }
+    elseif($upgradeStatus.UpgradeState -eq "Failed")
+    {
+        Write-Error "Upgrade Failed."
+    }
+}
