fa82270e1fd56e5aa44513d4e7e558e34c4cba64
Sreedhar Pelluru <spelluru@microsoft.com>
2017-09-11 18:02:08 -0400

    created the data-factory folder with the first sample

---
+++ data-factory/copy-from-azure-blob-to-blob/copy-from-azure-blob-to-blob.ps1
@@ -0,0 +1,165 @@
+﻿powershell Set-ExecutionPolicy Unrestricted -Scope CurrentUser
+
+# Set variables with your own values
+$resourceGroupName = "<Name of the resource group>"
+$dataFactoryName = "<Name of the data factory. Must be globally unique>"
+$dataFactoryRegion = "East US" # Currently, you can create data factories only in East US region. Data stores and computes used by a data factory can be in other regions. 
+$storageAccountName = "<Name of your Azure Storage account>"
+$storageAccountKey = "<Key for your Azure Storage account>"
+$sourceBlobPath = "<blob container>/<input folder>"
+$sinkBlobPath = "<bloblcontainer>/<output folder>"
+
+# Login to your Azure account
+Login-AzureRmAccount
+
+# Create a resource group
+New-AzureRmResourceGroup -Name $resourceGroupName -Location $dataFactoryRegion
+
+# Create a data factory
+$df = New-AzureRmDataFactoryV2 -ResourceGroupName $resourceGroupName -Location $dataFactoryRegion -Name $dataFactoryName -LoggingStorageAccountName $storageAccountName  -LoggingStorageAccountKey $storageAccountKey
+
+# Create a storage linked service in the data factory
+$storageLinkedServiceDefinition = @"
+{
+    "name": "AzureStorageLinkedService",
+    "properties": {
+        "type": "AzureStorage",
+        "typeProperties": {
+            "connectionString": {
+                "value": "DefaultEndpointsProtocol=https;AccountName=$storageAccountName;AccountKey=$storageAccountKey",
+                "type": "SecureString"
+            }
+        }
+    }
+}
+"@
+$storageLinkedServiceDefinition | Out-File c:\StorageLinkedService.json
+New-AzureRmDataFactoryV2LinkedService -DataFactory $df -Name "AzureStorageLinkedService" -File c:\StorageLinkedService.json
+
+# Create a dataset in the data factory
+$datasetDefiniton = @"
+{
+    "name": "BlobDataset",
+    "properties": {
+        "type": "AzureBlob",
+        "typeProperties": {
+            "folderPath": {
+                "value": "@{dataset().path}",
+                "type": "Expression"
+            }
+        },
+        "linkedServiceName": {
+            "referenceName": "AzureStorageLinkedService",
+            "type": "LinkedServiceReference"
+        },
+        "parameters": {
+            "path": {
+                "type": "String"
+            }
+        }
+    }
+}
+"@
+$datasetDefiniton | Out-File c:\BlobDataset.json
+New-AzureRmDataFactoryV2Dataset -DataFactory $df -Name "BlobDataset" -File "c:\BlobDataset.json"
+
+# Create a pipeline in the data factory
+$pipelineDefinition = @"
+{
+    "name": "Adfv2QuickStartPipeline",
+    "properties": {
+        "activities": [
+            {
+                "name": "CopyFromBlobToBlob",
+                "type": "Copy",
+                "inputs": [
+                    {
+                        "referenceName": "BlobDataset",
+                        "parameters": {
+                            "path": "@pipeline().parameters.inputPath"
+                        },
+                    "type": "DatasetReference"
+                    }
+                ],
+                "outputs": [
+                    {
+                        "referenceName": "BlobDataset",
+                        "parameters": {
+                            "path": "@pipeline().parameters.outputPath"
+                        },
+                        "type": "DatasetReference"
+                    }
+                ],
+                "typeProperties": {
+                    "source": {
+                        "type": "BlobSource"
+                    },
+                    "sink": {
+                        "type": "BlobSink"
+                    }
+                }
+            }
+        ],
+        "parameters": {
+            "inputPath": {
+                "type": "String"
+            },
+            "outputPath": {
+                "type": "String"
+            }
+        }
+    }
+}
+"@
+$pipelineDefinition | Out-File c:\CopyPipeline.json
+New-AzureRmDataFactoryV2Pipeline -DataFactory $df -Name "CopyPipeline" -File "c:\CopyPipeline.json"
+
+# Create pipeline parameters for a run
+$pipelineParameters = @"
+{
+    "inputPath": "$sourceBlobPath",
+    "outputPath": "$sinkBlobPath"
+}
+"@
+$pipelineParameters | Out-File c:\PipelineParameters.json
+
+# Create a pipeline run by using parameters
+$runId = New-AzureRmDataFactoryV2PipelineRun -DataFactory $df -PipelineName "CopyPipeline" -ParameterFile c:\PipelineParameters.json
+
+# Check the pipeline run status until it finishes the copy operation
+while ($True) {
+    $run = Get-AzureRmDataFactoryV2PipelineRun -DataFactory $df -RunId $runId -ErrorAction Stop
+    Write-Host  "Pipeline run status: " $run.Status -foregroundcolor "Yellow"
+
+    if ($run.Status -eq "InProgress") {
+        Start-Sleep -Seconds 15
+    }
+    else {
+        $run
+        break
+    }
+}
+
+# Get the activity run details 
+$result = Get-AzureRmDataFactoryV2ActivityRun -DataFactory $df `
+    -PipelineName "Adfv2QuickStartPipeline" `
+    -PipelineRunId $runId `
+    -RunStartedAfter (Get-Date).AddMinutes(-10) `
+    -RunStartedBefore (Get-Date).AddMinutes(10) `
+    -ErrorAction Stop
+
+$result
+
+if ($run.Status -eq "Succeeded") {`
+    $result.Output -join "`r`n"`
+}`
+else {`
+    $result.Error -join "`r`n"`
+}
+
+# To remove the data factory from the resource gorup
+# Remove-AzureRmDataFactoryV2 -Name $dataFactoryName -ResourceGroupName $resourceGroupName
+# 
+# To remove the whole resource group
+# Remove-AzureRmResourceGroup  -Name $resourceGroupName
+
---
+++ data-factory/deploy-managed-dedicated-integration-runtime/deploy-managed-dedicated-integration-runtime.ps1
@@ -0,0 +1,99 @@
+Set-ExecutionPolicy Unrestricted -Scope CurrentUser
+
+##### Managed-dedicated integration runtime specifications
+
+# If your inputs contain PSH special characters, e.g. "$", please precede it with the escape character "`" like "`$". 
+
+# Azure Data Factory information
+$SubscriptionName = "<your azure subscription name>"
+$ResourceGroupName = "<azure resource group name>"
+$DataFactoryName = "<globablly unique name for your data factory>"
+$DataFactoryLocation = "EastUS" # data factory v2 can be created only in east us region. 
+$DataFactoryLoggingStorageAccountName = "<storage account name>"
+$DataFactoryLoggingStorageAccountKey = "<storage account key>"
+
+# Managed-dedicated integration runtime information
+$MDIRName = "<name of managed-dedicated integration runtime>"
+$MDIRDescription = "This is my managed-dedicated integration runtime instance"
+$MDIRLocation = "EastUS" # only East US|North Europe are supported
+$MDIRNodeSize = "Standard_A4_v2" # currently, only Standard_A4_v2|Standard_A8_v2|Standard_D1_v2|Standard_D2_v2|Standard_D3_v2|Standard_D4_v2 are supported 
+$MDIRNodeNumber = 2 # only 1-10 nodes are supported
+$MDIRMaxParallelExecutionsPerNode = 2 # only 1-8 parallel executions per node are supported
+$VnetId = "" # OPTIONAL: only classic VNet is supported
+$SubnetName = "" # OPTIONAL: only classic VNet is supported
+
+# SSISDB information
+$SSISDBServerEndpoint = "<your azure sql server name>.database.windows.net"
+$SSISDBServerAdminUserName = "<sql server admin user ID>"
+$SSISDBServerAdminPassword = "<sql server admin password>"
+$SSISDBPricingTier = "<your azure sql database pricing tier, e.g. S0, S3, or leave it empty for azure sql managed instance>" # Not applicable for Azure SQL MI
+
+##### End of managed-dedicated integration runtime specifications ##### 
+
+$SSISDBConnectionString = "Data Source=" + $SSISDBServerEndpoint + ";User ID="+ $SSISDBServerAdminUserName +";Password="+ $SSISDBServerAdminPassword
+
+##### Validate Azure SQL DB/MI server ##### 
+
+$sqlConnection = New-Object System.Data.SqlClient.SqlConnection $SSISDBConnectionString;
+Try
+{
+    $sqlConnection.Open();
+}
+Catch [System.Data.SqlClient.SqlException]
+{
+    Write-Warning "Cannot connect to your Azure SQL DB logical server/Azure SQL MI server, exception: $_"  ;
+    Write-Warning "Please make sure the server you specified has already been created. Do you want to proceed? [Y/N]"
+    $yn = Read-Host
+    if(!($yn -ieq "Y"))
+    {
+        Return;
+    } 
+}
+
+##### Login and select Azure subscription #####
+
+Login-AzureRmAccount
+Select-AzureRmSubscription -SubscriptionName $SubscriptionName
+
+##### Automatically configure VNet permissions/settings for managed-dedicated integration runtime to join ##### 
+
+# Register to Azure Batch resource provider
+if(![string]::IsNullOrEmpty($VnetId) -and ![string]::IsNullOrEmpty($SubnetName))
+{
+    $BatchObjectId = (Get-AzureRmADServicePrincipal -ServicePrincipalName "MicrosoftAzureBatch").Id
+    Register-AzureRmResourceProvider -ProviderNamespace Microsoft.Batch
+    while(!(Get-AzureRmResourceProvider -ProviderNamespace "Microsoft.Batch").RegistrationState.Contains("Registered"))
+    {
+	Start-Sleep -s 10
+    }
+    # Assign VM contributor role to Microsoft.Batch
+    New-AzureRmRoleAssignment -ObjectId $BatchObjectId -RoleDefinitionName "Classic Virtual Machine Contributor" -Scope $VnetId
+}
+
+##### Provision data factory + managed-dedicated integration runtime ##### 
+New-AzureRmResourceGroup -Location $DataFactoryLocation -Name $ResourceGroupName
+Register-AzureRmResourceProvider -ProviderNamespace Microsoft.DataFactory
+New-AzureRmDataFactoryV2 -Location $DataFactoryLocation -LoggingStorageAccountName $DataFactoryLoggingStorageAccountName -LoggingStorageAccountKey $DataFactoryLoggingStorageAccountKey -Name $DataFactoryName -ResourceGroupName $ResourceGroupName 
+New-AzureRmDataFactoryV2IntegrationRuntime -DataFactoryName $DataFactoryName -Name $MDIRName -ResourceGroupName $ResourceGroupName -Type Managed -CatalogServerEndpoint $SSISDBServerEndpoint -CatalogAdminUserName $SSISDBServerAdminUserName -CatalogAdminPassword $SSISDBServerAdminPassword -CatalogPricingTier $SSISDBPricingTier -Description $MDIRDescription -Location $MDIRLocation -NodeSize $MDIRNodeSize -NumberOfNodes $MDIRNodeNumber -MaxParallelExecutionsPerNode $MDIRMaxParallelExecutionsPerNode -VnetId $VnetId -Subnet $SubnetName
+write-host("##### Starting #####")
+Start-AzureRmDataFactoryV2IntegrationRuntime -DataFactoryName $DataFactoryName -Name $MDIRName -ResourceGroupName $ResourceGroupName -Sync -Force
+write-host("##### Completed #####")
+write-host("If any cmdlet is unsuccessful, please consider using -Debug option for diagnostics.")
+
+##### Get managed-dedicated integration runtime status #####
+
+#Get-AzureRmDataFactoryV2IntegrationRuntime -DataFactoryName $DataFactoryName -Name $MDIRName -ResourceGroupName $ResourceGroupName
+#Get-AzureRmDataFactoryV2IntegrationRuntimeStatus -DataFactoryName $DataFactoryName -Name $MDIRName -ResourceGroupName $ResourceGroupName
+
+##### Reconfigure managed-dedicated integration runtime, e.g. scale out from 2 to 5 nodes #####
+
+#Stop-AzureRmDataFactoryV2IntegrationRuntime -DataFactoryName $DataFactoryName -Name $MDIRName -ResourceGroupName $ResourceGroupName 
+#Set-AzureRmDataFactoryV2IntegrationRuntime -DataFactoryName $DataFactoryName -Name $MDIRName -ResourceGroupName $ResourceGroupName -NumberOfNodes 5
+#Start-AzureRmDataFactoryV2IntegrationRuntime -DataFactoryName $DataFactoryName -Name $MDIRName -ResourceGroupName $ResourceGroupName -Sync
+
+##### Clean up ######
+
+#Stop-AzureRmDataFactoryV2IntegrationRuntime -DataFactoryName $DataFactoryName -Name $MDIRName -ResourceGroupName $ResourceGroupName -Force
+#Remove-AzureRmDataFactoryV2IntegrationRuntime -DataFactoryName $DataFactoryName -Name $MDIRName -ResourceGroupName $ResourceGroupName -Force
+#Remove-AzureRmDataFactoryV2 -Name $DataFactoryName -ResourceGroupName $ResourceGroupName -Force
+#Remove-AzureRmResourceGroup -Name $ResourceGroupName -Force
