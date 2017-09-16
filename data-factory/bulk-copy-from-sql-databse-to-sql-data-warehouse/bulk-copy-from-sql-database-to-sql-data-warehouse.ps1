1d689f4d0232770796b0e326f76b6cc500666245
Sreedhar Pelluru <spelluru@microsoft.com>
2017-09-16 17:38:02 -0400

    adding the bulk copy sample

---
+++ data-factory/bulk-copy-from-sql-databse-to-sql-data-warehouse/bulk-copy-from-sql-database-to-sql-data-warehouse.ps1
@@ -0,0 +1,340 @@
+﻿# Set variables with your own values
+$resourceGroup = "<resource group name>"
+$dataFactory = "<data factory name>" # Name of the data factory must be globally unique
+$dataFactoryLocation = "East US"
+
+$azureSqlServer = "<azure sql server name>"
+$azureSqlServerUser = "<azure sql server user>"
+$azureSqlServerUserPassword = "<azure sql server password>"
+$azureSqlDatabase = "<source azure sql database name>"
+$azureSqlDataWarehouse = "<sink azure sql data warehouse name>"
+
+$azureStorageAccount = "<azure storage accoutn name>"
+$azureStorageAccountKey = "<azure storage account key>"
+
+# No need to change values for these variables
+$azureSqlDatabaseLinkedService = "AzureSqlDatabaseLinkedService"
+$azureSqlDataWarehouseLinkedService = "AzureSqlDataWarehouseLinkedService"
+$azureStorageLinkedService = "AzureStorageLinkedService"
+$azureSqlDatabaseDataset = "AzureSqlDatabaseDataset"
+$azureSqlDataWarehouseDataset = "AzureSqlDataWarehouseDataset"
+$IterateAndCopySQLTablesPipeline = "IterateAndCopySQLTables"
+$pipelineGetTableListAndTriggerCopyData = "GetTableListAndTriggerCopyData"
+
+
+# create a resource gorup
+New-AzureRmResourceGroup -Name $resourceGroup -Location $dataFactoryLocation
+
+# create a data factory
+$df = Set-AzureRmDataFactoryV2 -ResourceGroupName $resourceGroup -Location $dataFactoryLocation -Name $dataFactory
+
+# create a linked service for Azure SQL Database (source)
+$azureSQLDatabaseLinkedServiceDefinition = @"
+{
+    "name": "$azureSqlDatabaseLinkedService",
+    "properties": {
+        "type": "AzureSqlDatabase",
+        "typeProperties": {
+            "connectionString": {
+                "type": "SecureString",
+                "value": "Server=tcp:$azureSqlServer.database.windows.net,1433;Database=$azureSqlDatabase;User ID=$azureSqlServerUser;Password=$azureSqlServerUserPassword;Trusted_Connection=False;Encrypt=True;Connection Timeout=30"
+            }
+        }
+    }
+}
+"@
+
+## IMPORTANT: store the JSON definition in a file that will be used by the Set-AzureRmDataFactoryV2LinkedService command. 
+$azureSQLDatabaseLinkedServiceDefinition | Out-File c:\$azureSqlDatabaseLinkedService.json
+
+## Creates an Azure Storage linked service
+Set-AzureRmDataFactoryV2LinkedService -DataFactory $df -Name $azureSqlDatabaseLinkedService -File c:\$azureSqlDatabaseLinkedService.json
+
+
+# create a linked service for Azure SQL Datawarehouse (sink)
+$azureSQLDataWarehouseLinkedServiceDefinition = @"
+{
+    "name": "$azureSqlDataWarehouseLinkedService",
+    "properties": {
+        "type": "AzureSqlDW",
+        "typeProperties": {
+            "connectionString": {
+                "type": "SecureString",
+                "value": "Server=tcp:$azureSqlServer.database.windows.net,1433;Database=$azureSqlDataWarehouse;User ID=$azureSqlServerUser;Password=$azureSqlServerUserPassword;Trusted_Connection=False;Encrypt=True;Connection Timeout=30"
+            }
+        }
+    }
+}
+"@
+
+## IMPORTANT: store the JSON definition in a file that will be used by the Set-AzureRmDataFactoryV2LinkedService command. 
+$azureSQLDataWarehouseLinkedServiceDefinition | Out-File c:\$azureSqlDataWarehouseLinkedService.json
+
+## Creates an linked service for Azure Storage Account. Interim storage to enable PolyBase
+Set-AzureRmDataFactoryV2LinkedService -DataFactory $df -Name $azureSqlDataWarehouseLinkedService -File c:\$azureSqlDataWarehouseLinkedService.json
+
+$storageLinkedServiceDefinition = @"
+{
+    "name": "$azureStorageLinkedService",
+    "properties": {
+        "type": "AzureStorage",
+        "typeProperties": {
+            "connectionString": {
+                "value": "DefaultEndpointsProtocol=https;AccountName=$azureStorageAccount;AccountKey=$azureStorageAccountKey",
+                "type": "SecureString"
+            }
+        }
+    }
+}
+"@
+
+## IMPORTANT: store the JSON definition in a file that will be used by the Set-AzureRmDataFactoryV2LinkedService command. 
+$storageLinkedServiceDefinition | Out-File c:\$azureStorageLinkedService.json
+
+## Creates an Azure Storage linked service
+Set-AzureRmDataFactoryV2LinkedService -DataFactory $df -Name $azureStorageLinkedService -File c:\$azureStorageLinkedService.json
+
+
+# create the input dataset (Azure SQL Database)
+$azureSqlDatabaseDatasetDefiniton = @"
+{
+    "name": "$azureSqlDatabaseDataset",
+    "properties": {
+        "type": "AzureSqlTable",
+        "linkedServiceName": {
+            "referenceName": "$azureSqlDatabaseLinkedService",
+            "type": "LinkedServiceReference"
+        },
+        "typeProperties": {
+            "tableName": "dummy"
+        }
+    }
+}
+"@
+
+## IMPORTANT: store the JSON definition in a file that will be used by the Set-AzureRmDataFactoryV2Dataset command. 
+$azureSqlDatabaseDatasetDefiniton | Out-File c:\$azureSqlDatabaseDataset.json
+
+## Create a dataset in the data factory
+Set-AzureRmDataFactoryV2Dataset -DataFactory $df -Name $azureSqlDatabaseDataset -File "c:\$azureSqlDatabaseDataset.json"
+
+
+# create the output dataset (Azure SQL Data Warehouse)
+$azureSqlDataWarehouseDatasetDefiniton = @"
+{
+    "name": "$azureSqlDataWarehouseDataset",
+    "properties": {
+        "type": "AzureSqlDWTable",
+        "linkedServiceName": {
+            "referenceName": "$azureSqlDataWarehouseLinkedService",
+            "type": "LinkedServiceReference"
+        },
+        "typeProperties": {
+            "tableName": {
+                "value": "@{dataset().DWTableName}",
+                "type": "Expression"
+            }
+        },
+        "parameters":{
+            "DWTableName":{
+                "type":"String"
+            }
+        }
+    }
+}
+"@
+
+## IMPORTANT: store the JSON definition in a file that will be used by the Set-AzureRmDataFactoryV2Dataset command. 
+$azureSqlDataWarehouseDatasetDefiniton | Out-File c:\$azureSqlDataWarehouseDataset.json
+
+## Create a dataset in the data factory
+Set-AzureRmDataFactoryV2Dataset -DataFactory $df -Name $azureSqlDataWarehouseDataset -File "c:\$azureSqlDataWarehouseDataset.json"
+
+# Create a pipeline in the data factory that copies data from source SQL Database to sink SQL Data Warehouse
+$pipelineDefinition = @"
+{
+    "name": "$IterateAndCopySQLTablesPipeline",
+    "properties": {
+        "activities": [
+            {
+                "name": "IterateSQLTables",
+                "type": "ForEach",
+                "typeProperties": {
+                    "isSequential": "false",
+                    "items": {
+                        "value": "@pipeline().parameters.tableList",
+                        "type": "Expression"
+                    },
+                    "activities": [
+                        {
+                            "name": "CopyData",
+                            "description": "Copy data from SQL database to SQL DW",
+                            "type": "Copy",
+                            "inputs": [
+                                {
+                                    "referenceName": "$azureSqlDatabaseDataset",
+                                    "type": "DatasetReference"
+                                }
+                            ],
+                            "outputs": [
+                                {
+                                    "referenceName": "$azureSqlDataWarehouseDataset",
+                                    "type": "DatasetReference",
+                                    "parameters": {
+                                        "DWTableName": "[@{item().TABLE_SCHEMA}].[@{item().TABLE_NAME}]"
+                                    }
+                                }
+                            ],
+                            "typeProperties": {
+                                "source": {
+                                    "type": "SqlSource",
+                                    "sqlReaderQuery": "SELECT * FROM [@{item().TABLE_SCHEMA}].[@{item().TABLE_NAME}]"
+                                },
+                                "sink": {
+                                    "type": "SqlDWSink",
+                                    "preCopyScript": "TRUNCATE TABLE [@{item().TABLE_SCHEMA}].[@{item().TABLE_NAME}]",
+                                    "allowPolyBase": true
+                                },
+                                "enableStaging": true,
+                                "stagingSettings": {
+                                    "linkedServiceName": {
+                                        "referenceName": "$azureStorageLinkedService",
+                                        "type": "LinkedServiceReference"
+                                    }
+                                }
+                            }
+                        }
+                    ]
+                }
+            }
+        ],
+        "parameters": {
+            "tableList": {
+                "type": "Object"
+            }
+        }
+    }
+}
+"@
+
+## IMPORTANT: store the JSON definition in a file that will be used by the Set-AzureRmDataFactoryV2Pipeline command. 
+$pipelineDefinition | Out-File c:\$IterateAndCopySQLTablesPipeline.json
+
+## Create a pipeline in the data factory
+Set-AzureRmDataFactoryV2Pipeline -DataFactory $df -Name $IterateAndCopySQLTablesPipeline -File "c:\$IterateAndCopySQLTablesPipeline.json"
+
+
+# Create a pipeline in the data factory that retrieves a list of tables and invokes the above pipeline for each table to be copied
+$pipeline2Definition = @"
+{
+    "name":"$pipelineGetTableListAndTriggerCopyData",
+    "properties":{
+        "activities":[
+            { 
+                "name": "LookupTableList",
+                "description": "Retrieve the table list from Azure SQL dataabse",
+                "type": "Lookup",
+                "typeProperties": {
+                    "source": {
+                        "type": "SqlSource",
+                        "sqlReaderQuery": "SELECT TABLE_SCHEMA, TABLE_NAME FROM information_schema.TABLES WHERE TABLE_TYPE = 'BASE TABLE' and TABLE_SCHEMA = 'SalesLT' and TABLE_NAME <> 'ProductModel'"
+                    },
+                    "dataset": {
+                        "referenceName": "$azureSqlDatabaseDataset",
+                        "type": "DatasetReference"
+                    },
+                    "firstRowOnly": false
+                }
+            },
+            {
+                "name": "TriggerCopy",
+                "type": "ExecutePipeline",
+                "typeProperties": {
+                    "parameters": {
+                        "tableList": {
+                            "value": "@activity('LookupTableList').output.value",
+                            "type": "Expression"
+                        }
+                    },
+                    "pipeline": {
+                        "referenceName": "$IterateAndCopySQLTablesPipeline",
+                        "type": "PipelineReference"
+                    },
+                    "waitOnCompletion": true
+                },
+                "dependsOn": [
+                    {
+                        "activity": "LookupTableList",
+                        "dependencyConditions": [
+                            "Succeeded"
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+}
+"@
+
+## IMPORTANT: store the JSON definition in a file that will be used by the Set-AzureRmDataFactoryV2Pipeline command. 
+$pipeline2Definition | Out-File c:\$pipelineGetTableListAndTriggerCopyData.json
+
+## Create a pipeline in the data factory
+Set-AzureRmDataFactoryV2Pipeline -DataFactory $df -Name $pipelineGetTableListAndTriggerCopyData -File "c:\$pipelineGetTableListAndTriggerCopyData.json"
+
+
+
+# Create a pipeline run 
+
+## JSON definition for dummy pipeline parameters
+$pipelineParameters = @"
+{
+    "dummy":  "b"
+}
+"@
+
+## IMPORTANT: store the JSON definition in a file that will be used by the Invoke-AzureRmDataFactoryV2PipelineRun command. 
+$pipelineParameters | Out-File c:\PipelineParameters.json
+
+# Create a pipeline run by using parameters
+$runId = Invoke-AzureRmDataFactoryV2PipelineRun -DataFactory $df -PipelineName $pipelineGetTableListAndTriggerCopyData -ParameterFile c:\PipelineParameters.json
+
+# Check the pipeline run status until it finishes the copy operation
+Start-Sleep -Seconds 30
+while ($True) {
+    $result = Get-AzureRmDataFactoryV2ActivityRun -DataFactory $df -PipelineRunId $runId -PipelineName $pipelineGetTableListAndTriggerCopyData -RunStartedAfter (Get-Date).AddMinutes(-30) -RunStartedBefore (Get-Date).AddMinutes(30)
+
+    if (($result | Where-Object { $_.Status -eq "InProgress" } | Measure-Object).count -ne 0) {
+        Write-Host "Pipeline run status: In Progress" -foregroundcolor "Yellow"
+        Start-Sleep -Seconds 30
+    }
+    else {
+        Write-Host "Pipeline '$pipelineGetTableListAndTriggerCopyData' run finished. Result:" -foregroundcolor "Yellow"
+        $result
+        break
+    }
+}
+
+# Get the activity run details 
+    $result = Get-AzureRmDataFactoryV2ActivityRun -DataFactory $df `
+        -PipelineName $pipelineGetTableListAndTriggerCopyData `
+        -PipelineRunId $runId `
+        -RunStartedAfter (Get-Date).AddMinutes(-10) `
+        -RunStartedBefore (Get-Date).AddMinutes(10) `
+        -ErrorAction Stop
+
+    $result
+
+    if ($result.Status -eq "Succeeded") {`
+        $result.Output -join "`r`n"`
+    }`
+    else {`
+        $result.Error -join "`r`n"`
+    }
+
+# To remove the data factory from the resource gorup
+Remove-AzureRmDataFactoryV2 -Name $dataFactory -ResourceGroupName $resourceGroup
+# 
+# To remove the whole resource group
+# Remove-AzureRmResourceGroup  -Name $resourceGroup
+
