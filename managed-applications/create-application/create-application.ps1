8738295e70f1ccacdab0a4cbbb82e567682d9cc5
Tom FitzMacken <tomfitz@microsoft.com>
2017-10-24 19:34:48 -0700

    add managed apps scripts (#90)
    * add managed apps scripts
    
    * added policy and get vms

---
+++ managed-applications/create-application/create-application.ps1
@@ -0,0 +1,15 @@
+# Create resource group
+New-AzureRmResourceGroup -Name applicationGroup -Location westcentralus
+
+# Get ID of managed application definition
+$appid=(Get-AzureRmManagedApplicationDefinition -ResourceGroupName appDefinitionGroup -Name ManagedStorage).ManagedApplicationDefinitionId
+
+# Create the managed application
+New-AzureRmManagedApplication `
+  -Name storageApp `
+  -Location westcentralus `
+  -Kind ServiceCatalog `
+  -ResourceGroupName applicationGroup `
+  -ManagedApplicationDefinitionId $appid `
+  -ManagedResourceGroupName "InfrastructureGroup" `
+  -Parameter "{`"storageAccountNamePrefix`": {`"value`": `"demostorage`"}, `"storageAccountType`": {`"value`": `"Standard_LRS`"}}"
