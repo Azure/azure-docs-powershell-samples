e249940a6e011eca97aa19690f02ab63e481f434
Julia Kornich <juliako@microsoft.com>
2017-10-27 15:40:22 -0700

    apim shell scripts (#93)
    * apim shell scripts
    
    * changed variable names

---
+++ api-management/add-user-and-get-subscription-key/add_a_user_and_get_a_subscriptionKey.ps1
@@ -0,0 +1,51 @@
+##########################################################
+#  Script to create a user in api management and get a subscription key
+###########################################################
+
+$random = (New-Guid).ToString().Substring(0,8)
+
+#Azure specific details
+$subscriptionId = "my-azure-subscription-id"
+
+# Api Management service specific details
+$apimServiceName = "apim-$random"
+$resourceGroupName = "apim-rg-$random"
+$location = "Japan East"
+$organisation = "Contoso"
+$adminEmail = "admin@contoso.com"
+
+# User specific details
+$userEmail = "user@contoso.com"
+$userFirstName = "userFirstName"
+$userLastName = "userLastName"
+$userPassword = "userPassword"
+$userNote = "fellow trying out my apim instance"
+$userState = "Active"
+
+# Subscription Name details
+$subscriptionName = "subscriptionName"
+$subscriptionState = "Active"
+
+# Set the context to the subscription Id where the cluster will be created
+Select-AzureRmSubscription -SubscriptionId $subscriptionId
+
+# Create a resource group.
+New-AzureRmResourceGroup -Name $resourceGroupName -Location $location
+
+# Create the Api Management service. Since the SKU is not specified, it creates a service with Developer SKU. 
+New-AzureRmApiManagement -ResourceGroupName $resourceGroupName -Name $apimServiceName -Location $location -Organization $organisation -AdminEmail $adminEmail
+
+# Create the api management context
+$context = New-AzureRmApiManagementContext -ResourceGroupName $resourceGroupName -ServiceName $apimServiceName
+
+# create a new user in api management
+$user = New-AzureRmApiManagementUser -Context $context -FirstName $userFirstName -LastName $userLastName `
+    -Password $userPassword -State $userState -Note $userNote -Email $userEmail
+
+# get the details of the 'Starter' product in api management, which is created by default
+$product = Get-AzureRmApiManagementProduct -Context $context -Title 'Starter' | Select-Object -First 1
+
+# generate a subscription key for the user to call apis which are part of the 'Starter' product
+New-AzureRmApiManagementSubscription -Context $context -UserId $user.UserId `
+    -ProductId $product.ProductId -Name $subscriptionName -State $subscriptionState
+
---
+++ api-management/create-apim-service/create_apim_service.ps1
@@ -0,0 +1,28 @@
+##########################################################
+#  Script to create a Developer SKU API Management Service
+#
+###########################################################
+
+$random = (New-Guid).ToString().Substring(0,8)
+
+#Azure specific details
+$subscriptionId = "my-azure-subscription-id"
+
+# Api Management service specific details
+$apimServiceName = "apim-$random"
+$resourceGroupName = "apim-rg-$random"
+$location = "Japan East"
+$organisation = "Contoso"
+$adminEmail = "admin@contoso.com"
+
+# Set the context to the subscription Id where the cluster will be created
+Select-AzureRmSubscription -SubscriptionId $subscriptionId
+
+# Create a resource group.
+New-AzureRmResourceGroup -Name $resourceGroupName -Location $location
+
+# Create the Api Management service. Since the SKU is not specified, it creates a service with Developer SKU. 
+New-AzureRmApiManagement -ResourceGroupName $resourceGroupName -Name $apimServiceName -Location $location -Organization $organisation -AdminEmail $adminEmail
+
+# Get an SSO token to login into the Service
+Get-AzureRmApiManagementSsoToken -ResourceGroupName $resourceGroupName -Name $apimServiceName
