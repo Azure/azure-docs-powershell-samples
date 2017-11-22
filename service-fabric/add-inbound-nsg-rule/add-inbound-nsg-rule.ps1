70d24017b911613a545d2ed58c45b6856c52f830
Ryan Wike <ryanwi@microsoft.com>
2017-11-22 12:01:21 -0800

    adding script

---
+++ service-fabric/add-inbound-nsg-rule/add-inbound-nsg-rule.ps1
@@ -0,0 +1,20 @@
+﻿Login-AzureRmAccount
+Get-AzureRmSubscription
+Set-AzureRmContext -SubscriptionId "yourSubscriptionID"
+
+$RGname="sfclustertutorialgroup"
+$port=8081
+$rulename="allowAppPort$port"
+$nsgname="sf-vnet-security"
+
+# Get the NSG resource
+$resource = Get-AzureRmResource | Where {$_.ResourceGroupName –eq $RGname -and $_.ResourceType -eq "Microsoft.Network/networkSecurityGroups"} 
+$nsg = Get-AzureRmNetworkSecurityGroup -Name $nsgname -ResourceGroupName $RGname
+
+# Add the inbound security rule.
+$nsg | Add-AzureRmNetworkSecurityRuleConfig -Name $rulename -Description "Allow app port" -Access Allow `
+    -Protocol * -Direction Inbound -Priority 3891 -SourceAddressPrefix "*" -SourcePortRange * `
+    -DestinationAddressPrefix * -DestinationPortRange $port
+
+# Update the NSG.
+$nsg | Set-AzureRmNetworkSecurityGroup
