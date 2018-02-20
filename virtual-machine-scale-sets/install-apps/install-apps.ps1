f2f25ab19fe72f07da30119ccdf5048fd4288984
Iain Foulds <iain.foulds@microsoft.com>
2018-02-20 15:54:21 -0800

    [VMSS] Adding MVC sample scripts

---
+++ virtual-machine-scale-sets/install-apps/install-apps.ps1
@@ -0,0 +1,122 @@
+# Define the resource group name and location
+$myResourceGroup = "myResourceGroup"
+$myLocation = "East US"
+
+# Provide your own secure password for use with the VM instances
+$securePassword = "P@ssword!"
+$adminUsername = "azureuser"
+
+# Create a resource group
+New-AzureRmResourceGroup -ResourceGroupName $myResourceGroup -Location $myLocation
+
+# Create a virtual network subnet
+$subnet = New-AzureRmVirtualNetworkSubnetConfig `
+  -Name "mySubnet" `
+  -AddressPrefix 10.0.0.0/24
+
+# Create a virtual network
+$vnet = New-AzureRmVirtualNetwork `
+  -ResourceGroupName $myResourceGroup `
+  -Name "myVnet" `
+  -Location $myLocation `
+  -AddressPrefix 10.0.0.0/16 `
+  -Subnet $subnet
+
+# Create a public IP address
+$publicIP = New-AzureRmPublicIpAddress `
+  -ResourceGroupName $myResourceGroup `
+  -Location $myLocation `
+  -AllocationMethod Static `
+  -Name "myPublicIP"
+
+# Create a frontend and backend IP pool
+$frontendIP = New-AzureRmLoadBalancerFrontendIpConfig `
+  -Name "myFrontEndPool" `
+  -PublicIpAddress $publicIP
+$backendPool = New-AzureRmLoadBalancerBackendAddressPoolConfig -Name "myBackEndPool"
+
+# Create a Network Address Translation (NAT) pool
+$inboundNATPool = New-AzureRmLoadBalancerInboundNatPoolConfig `
+  -Name "myRDPRule" `
+  -FrontendIpConfigurationId $frontendIP.Id `
+  -Protocol TCP `
+  -FrontendPortRangeStart 50001 `
+  -FrontendPortRangeEnd 50010 `
+  -BackendPort 3389
+
+# Create the load balancer
+$lb = New-AzureRmLoadBalancer `
+  -ResourceGroupName $myResourceGroup `
+  -Name "myLoadBalancer" `
+  -Location $myLocation `
+  -FrontendIpConfiguration $frontendIP `
+  -BackendAddressPool $backendPool `
+  -InboundNatPool $inboundNATPool
+
+# Create IP address configurations
+$ipConfig = New-AzureRmVmssIpConfig `
+  -Name "myIPConfig" `
+  -LoadBalancerBackendAddressPoolsId $lb.BackendAddressPools[0].Id `
+  -LoadBalancerInboundNatPoolsId $inboundNATPool.Id `
+  -SubnetId $vnet.Subnets[0].Id
+
+# Create a config object
+$vmssConfig = New-AzureRmVmssConfig `
+  -Location $myLocation `
+  -SkuCapacity 2 `
+  -SkuName "Standard_DS2" `
+  -UpgradePolicyMode Automatic
+
+# Reference a virtual machine image from the gallery
+Set-AzureRmVmssStorageProfile $vmssConfig `
+  -ImageReferencePublisher "MicrosoftWindowsServer" `
+  -ImageReferenceOffer "WindowsServer" `
+  -ImageReferenceSku "2016-Datacenter" `
+  -ImageReferenceVersion "latest"
+
+# Set up information for authenticating with the virtual machine
+Set-AzureRmVmssOsProfile $vmssConfig `
+  -AdminUsername $adminUsername `
+  -AdminPassword $securePassword `
+  -ComputerNamePrefix "myVM"
+
+# Attach the virtual network to the config object
+Add-AzureRmVmssNetworkInterfaceConfiguration `
+  -VirtualMachineScaleSet $vmssConfig `
+  -Name "network-config" `
+  -Primary $true `
+  -IPConfiguration $ipConfig
+
+# Create the scale set with the config object (this step might take a few minutes)
+New-AzureRmVmss `
+  -ResourceGroupName $myResourceGroup `
+  -Name "myScaleSet" `
+  -VirtualMachineScaleSet $vmssConfig
+
+# Create a configuration object to store the Custom Script Extension definition
+$customConfig = @{
+"fileUris" = (,"https://raw.githubusercontent.com/Azure-Samples/compute-automation-configurations/master/automate-iis.ps1");
+"commandToExecute" = "powershell -ExecutionPolicy Unrestricted -File automate-iis.ps1"
+}
+
+# Get information about the scale set
+$vmss = Get-AzureRmVmss `
+          -ResourceGroupName $myResourceGroup `
+          -VMScaleSetName "myScaleSet"
+
+# Add the Custom Script Extension to install IIS and configure basic website
+$vmss = Add-AzureRmVmssExtension `
+  -VirtualMachineScaleSet $vmss `
+  -Name "customScript" `
+  -Publisher "Microsoft.Compute" `
+  -Type "CustomScriptExtension" `
+  -TypeHandlerVersion 1.8 `
+  -Setting $customConfig
+
+# Update the scale set and apply the Custom Script Extension to the VM instances
+Update-AzureRmVmss `
+  -ResourceGroupName $myResourceGroup `
+  -Name "myScaleSet" `
+  -VirtualMachineScaleSet $vmss
+
+Get-AzureRmPublicIpAddress -ResourceGroupName $myResourceGroup | Select IpAddress
