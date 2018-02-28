c88b51f7c2e6a58f9d5f513d513178c482027370
Iain Foulds <iain.foulds@microsoft.com>
2018-02-27 16:18:32 -0800

    [VMSS] Adding MVC sample scripts (#111)
    * Updating for new PowerShell cmdlets
    
    * [VMSS] Adding MVC sample scripts

---
+++ virtual-machine-scale-sets/auto-scale-host-metrics/auto-scale-host-metrics.ps1
@@ -0,0 +1,142 @@
+# Define the resource group name and location
+$mySubscriptionId = (Get-AzureRmSubscription)[0].Id
+$myResourceGroup = "myResourceGroup2"
+$myScaleSet = "myScaleSet"
+$myLocation = "East US"
+
+# Provide your own secure password for use with the VM instances
+$securePassword = "P@ssword!"
+$adminUsername = "azureuser"
+
+# Create a resource group
+New-AzureRmResourceGroup -ResourceGroupName $myResourceGroup -Location $location
+
+# Create a virtual network subnet
+$subnet = New-AzureRmVirtualNetworkSubnetConfig `
+  -Name "mySubnet" `
+  -AddressPrefix 10.0.0.0/24
+
+# Create a virtual network
+$vnet = New-AzureRmVirtualNetwork `
+  -ResourceGroupName $myResourceGroup `
+  -Name "myVnet" `
+  -Location $location `
+  -AddressPrefix 10.0.0.0/16 `
+  -Subnet $subnet
+
+# Create a public IP address
+$publicIP = New-AzureRmPublicIpAddress `
+  -ResourceGroupName $myResourceGroup `
+  -Location $location `
+  -AllocationMethod Static `
+  -Name "myPublicIP"
+
+# Create a frontend and backend IP pool
+$frontendIP = New-AzureRmLoadBalancerFrontendIpConfig `
+  -Name "myFrontEndPool" `
+  -PublicIpAddress $publicIP
+$backendPool = New-AzureRmLoadBalancerBackendAddressPoolConfig -Name "myBackEndPool"
+
+# Create a Network Address Translation (NAT) pool
+$inboundNATPool = New-AzureRmLoadBalancerInboundNatPoolConfig `
+  -Name "myRDPRule" `
+  -FrontendIpConfigurationId $frontendIP.Id `
+  -Protocol TCP `
+  -FrontendPortRangeStart 50001 `
+  -FrontendPortRangeEnd 50010 `
+  -BackendPort 3389
+
+# Create the load balancer
+$lb = New-AzureRmLoadBalancer `
+  -ResourceGroupName $myResourceGroup `
+  -Name "myLoadBalancer" `
+  -Location $location `
+  -FrontendIpConfiguration $frontendIP `
+  -BackendAddressPool $backendPool `
+  -InboundNatPool $inboundNATPool
+
+# Create IP address configurations
+$ipConfig = New-AzureRmVmssIpConfig `
+  -Name "myIPConfig" `
+  -LoadBalancerBackendAddressPoolsId $lb.BackendAddressPools[0].Id `
+  -LoadBalancerInboundNatPoolsId $inboundNATPool.Id `
+  -SubnetId $vnet.Subnets[0].Id
+
+# Create a config object
+$vmssConfig = New-AzureRmVmssConfig `
+  -Location $location `
+  -SkuCapacity 2 `
+  -SkuName "Standard_DS2" `
+  -UpgradePolicyMode Automatic
+
+# Reference a virtual machine image from the gallery
+Set-AzureRmVmssStorageProfile $vmssConfig `
+  -ImageReferencePublisher "MicrosoftWindowsServer" `
+  -ImageReferenceOffer "WindowsServer" `
+  -ImageReferenceSku "2016-Datacenter" `
+  -ImageReferenceVersion "latest"
+
+# Set up information for authenticating with the virtual machine
+Set-AzureRmVmssOsProfile $vmssConfig `
+  -AdminUsername $adminUsername `
+  -AdminPassword $securePassword `
+  -ComputerNamePrefix "myVM"
+
+# Attach the virtual network to the config object
+Add-AzureRmVmssNetworkInterfaceConfiguration `
+  -VirtualMachineScaleSet $vmssConfig `
+  -Name "network-config" `
+  -Primary $true `
+  -IPConfiguration $ipConfig
+
+# Create the scale set with the config object (this step might take a few minutes)
+New-AzureRmVmss `
+  -ResourceGroupName $myResourceGroup `
+  -Name "myScaleSet" `
+  -VirtualMachineScaleSet $vmssConfig
+
+# Create an autoscale rule to increase the number of VM instances by 3 when the average CPU load over a 5-minute window
+# is greater than 70%
+$myRuleScaleOut = New-AzureRmAutoscaleRule `
+  -MetricName "Percentage CPU" `
+  -MetricResourceId /subscriptions/$mySubscriptionId/resourceGroups/$myResourceGroup/providers/Microsoft.Compute/virtualMachineScaleSets/$myScaleSet `
+  -TimeGrain 00:01:00 `
+  -MetricStatistic "Average" `
+  -TimeWindow 00:05:00 `
+  -Operator "GreaterThan" `
+  -Threshold 70 `
+  -ScaleActionDirection "Increase" `
+  –ScaleActionScaleType "ChangeCount" `
+  -ScaleActionValue 3 `
+  -ScaleActionCooldown 00:05:00
+
+# Create an autoscale rule to decrease the number of VM instances by 1 when the average CPU load over a 5-minute window
+# is less than 30%
+$myRuleScaleIn = New-AzureRmAutoscaleRule `
+  -MetricName "Percentage CPU" `
+  -MetricResourceId /subscriptions/$mySubscriptionId/resourceGroups/$myResourceGroup/providers/Microsoft.Compute/virtualMachineScaleSets/$myScaleSet `
+  -Operator "LessThan" `
+  -MetricStatistic "Average" `
+  -Threshold 30 `
+  -TimeGrain 00:01:00 `
+  -TimeWindow 00:05:00 `
+  -ScaleActionCooldown 00:05:00 `
+  -ScaleActionDirection "Decrease" `
+  –ScaleActionScaleType "ChangeCount" `
+  -ScaleActionValue 1
+
+# Create the autoscale profle that includes the scale out and scale in rules
+$myScaleProfile = New-AzureRmAutoscaleProfile `
+  -DefaultCapacity 2  `
+  -MaximumCapacity 10 `
+  -MinimumCapacity 2 `
+  -Rules $myRuleScaleOut,$myRuleScaleIn `
+  -Name "autoprofile"
+
+# Apply the autoscale profile to the virtual machine scale set
+Add-AzureRmAutoscaleSetting `
+  -Location $myLocation `
+  -Name "autosetting" `
+  -ResourceGroup $myResourceGroup `
+  -TargetResourceId /subscriptions/$mySubscriptionId/resourceGroups/$myResourceGroup/providers/Microsoft.Compute/virtualMachineScaleSets/$myScaleSet `
+  -AutoscaleProfiles $myScaleProfile
