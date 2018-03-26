676d36d7bc05c7c99b3a63857366d4ef0f54e114
Iain Foulds <iain.foulds@microsoft.com>
2018-03-26 16:39:58 -0700

    [VMSS] Updating for new PowerShell experience (#116)
    * Updating for new PowerShell cmdlets
    
    * [VMSS] Updating for new PowerShell experience

--- virtual-machine-scale-sets/use-data-disks/use-data-disks.ps1
+++ virtual-machine-scale-sets/use-data-disks/use-data-disks.ps1
@@ -1,130 +1,27 @@
-# Define the resource group name and location
-$myResourceGroup = "myResourceGroup"
-$myLocation = "East US"
-
 # Provide your own secure password for use with the VM instances
-$securePassword = "P@ssword!"
-$adminUsername = "azureuser"
+$cred = Get-Credential
 
-# Create a resource group
-New-AzureRmResourceGroup -ResourceGroupName $myResourceGroup -Location $myLocation
-
-# Create a virtual network subnet
-$subnet = New-AzureRmVirtualNetworkSubnetConfig `
-  -Name "mySubnet" `
-  -AddressPrefix 10.0.0.0/24
-
-# Create a virtual network
-$vnet = New-AzureRmVirtualNetwork `
-  -ResourceGroupName $myResourceGroup `
-  -Name "myVnet" `
-  -Location $myLocation `
-  -AddressPrefix 10.0.0.0/16 `
-  -Subnet $subnet
-
-# Create a public IP address
-$publicIP = New-AzureRmPublicIpAddress `
-  -ResourceGroupName $myResourceGroup `
-  -Location $myLocation `
-  -AllocationMethod Static `
-  -Name "myPublicIP"
-
-# Create a frontend and backend IP pool
-$frontendIP = New-AzureRmLoadBalancerFrontendIpConfig `
-  -Name "myFrontEndPool" `
-  -PublicIpAddress $publicIP
-$backendPool = New-AzureRmLoadBalancerBackendAddressPoolConfig -Name "myBackEndPool"
-
-# Create a Network Address Translation (NAT) pool
-$inboundNATPool = New-AzureRmLoadBalancerInboundNatPoolConfig `
-  -Name "myRDPRule" `
-  -FrontendIpConfigurationId $frontendIP.Id `
-  -Protocol TCP `
-  -FrontendPortRangeStart 50001 `
-  -FrontendPortRangeEnd 50010 `
-  -BackendPort 3389
-
-# Create the load balancer
-$lb = New-AzureRmLoadBalancer `
-  -ResourceGroupName $myResourceGroup `
-  -Name "myLoadBalancer" `
-  -Location $myLocation `
-  -FrontendIpConfiguration $frontendIP `
-  -BackendAddressPool $backendPool `
-  -InboundNatPool $inboundNATPool
-
-# Create IP address configurations
-$ipConfig = New-AzureRmVmssIpConfig `
-  -Name "myIPConfig" `
-  -LoadBalancerBackendAddressPoolsId $lb.BackendAddressPools[0].Id `
-  -LoadBalancerInboundNatPoolsId $inboundNATPool.Id `
-  -SubnetId $vnet.Subnets[0].Id
-
-# Create a config object
-$vmssConfig = New-AzureRmVmssConfig `
-  -Location $myLocation `
-  -SkuCapacity 2 `
-  -SkuName "Standard_DS2" `
-  -UpgradePolicyMode Automatic
-
-# Reference a virtual machine image from the gallery
-Set-AzureRmVmssStorageProfile $vmssConfig `
-  -ImageReferencePublisher "MicrosoftWindowsServer" `
-  -ImageReferenceOffer "WindowsServer" `
-  -ImageReferenceSku "2016-Datacenter" `
-  -ImageReferenceVersion "latest"
-
-# Set up information for authenticating with the virtual machine
-Set-AzureRmVmssOsProfile $vmssConfig `
-  -AdminUsername $adminUsername `
-  -AdminPassword $securePassword `
-  -ComputerNamePrefix "myVM"
-
-# Attach the virtual network to the config object
-Add-AzureRmVmssNetworkInterfaceConfiguration `
-  -VirtualMachineScaleSet $vmssConfig `
-  -Name "network-config" `
-  -Primary $true `
-  -IPConfiguration $ipConfig
-
-# Add the first data disk to the scale set config
-Add-AzureRmVmssDataDisk `
-  -VirtualMachineScaleSet $vmssConfig `
-  -CreateOption Empty `
-  -Lun 1 `
-  -DiskSizeGB 64
-
-# Add the second data disk to the scale set config
-Add-AzureRmVmssDataDisk `
-  -VirtualMachineScaleSet $vmssConfig `
-  -CreateOption Empty `
-  -Lun 2 `
-  -DiskSizeGB 128
-
-# Create the scale set with the config object (this step might take a few minutes)
+# Create a virtual machine scale set and supporting resources
+# A resource group, virtual network, load balancer, and NAT rules are automatically
+# created if they do not already exist
 New-AzureRmVmss `
-  -ResourceGroupName $myResourceGroup `
-  -Name "myScaleSet" `
-  -VirtualMachineScaleSet $vmssConfig
+  -ResourceGroupName "myResourceGroup" `
+  -VMScaleSetName "myScaleSet" `
+  -Location "EastUS" `
+  -VirtualNetworkName "myVnet" `
+  -SubnetName "mySubnet" `
+  -PublicIpAddressName "myPublicIPAddress" `
+  -LoadBalancerName "myLoadBalancer" `
+  -Credential $cred `
+  -UpgradePolicy "Automatic" `
+  -DataDiskSizeGb 64,128
 
 # Get scale set object
 $vmss = Get-AzureRmVmss `
-          -ResourceGroupName $myResourceGroup `
+          -ResourceGroupName "myResourceGroup" `
           -VMScaleSetName "myScaleSet"
 
-# Attach a 128 GB data disk to LUN 3
-Add-AzureRmVmssDataDisk `
-  -VirtualMachineScaleSet $vmss `
-  -CreateOption Empty `
-  -Lun 3 `
-  -DiskSizeGB 128
-
-# Update the scale set to apply the change
-Update-AzureRmVmss `
-  -ResourceGroupName $myResourceGroup `
-  -Name "myScaleSet" `
-  -VirtualMachineScaleSet $vmss
-
+# Set the URI of a custom script that prepares any attached disks
 $publicSettings = @{
   "fileUris" = (,"https://raw.githubusercontent.com/Azure-Samples/compute-automation-configurations/master/prepare_vm_disks.ps1");
   "commandToExecute" = "powershell -ExecutionPolicy Unrestricted -File prepare_vm_disks.ps1"
@@ -140,6 +37,6 @@ Add-AzureRmVmssExtension -VirtualMachineScaleSet $vmss `
 
 # Update the scale set and apply the Custom Script Extension to the VM instances
 Update-AzureRmVmss `
-  -ResourceGroupName $myResourceGroup `
+  -ResourceGroupName "myResourceGroup" `
   -Name "myScaleSet" `
   -VirtualMachineScaleSet $vmss
