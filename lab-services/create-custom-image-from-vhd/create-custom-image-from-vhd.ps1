d86ff5f29fec8bff6efb6423a3259229d9ab43f9
spelluru <spelluru@microsoft.com>
2018-04-10 21:17:58 -0400

    first PS sample

---
+++ lab-services/create-custom-image-from-vhd/create-custom-image-from-vhd.ps1
@@ -0,0 +1,26 @@
+﻿
+# Select the desired Azure subscription. 
+$subscriptionId = '<Specify your subscription ID here>'
+Select-AzureRmSubscription -SubscriptionId $subscriptionId
+
+# Get the lab object.
+$labRg = '<Specify your lab resource group name here>'
+$labName = '<Specify your lab name here>'
+$lab = Get-AzureRmResource -ResourceId ('/subscriptions/' + $subscriptionId + '/resourceGroups/' + $labRg + '/providers/Microsoft.DevTestLab/labs/' + $labName)
+
+# Get the lab storage account and lab storage account key values.
+$labStorageAccount = Get-AzureRmResource -ResourceId $lab.Properties.defaultStorageAccount 
+$labStorageAccountKey = (Get-AzureRmStorageAccountKey -ResourceGroupName $labStorageAccount.ResourceGroupName -Name $labStorageAccount.ResourceName)[0].Value
+
+# Set the URI of the VHD file.  
+$vhdUri = '<Specify the VHD URI here>'
+
+# Set the custom image name and description values.
+$customImageName = '<Specify the custom image name>'
+$customImageDescription = '<Specify the custom image description>'
+
+# Set up the parameters object.
+$parameters = @{existingLabName="$($lab.Name)"; existingVhdUri=$vhdUri; imageOsType='windows'; isVhdSysPrepped=$false; imageName=$customImageName; imageDescription=$customImageDescription}
+
+# Create the custom image. 
+New-AzureRmResourceGroupDeployment -ResourceGroupName $lab.ResourceGroupName -Name CreateCustomImage -TemplateUri 'https://raw.githubusercontent.com/Azure/azure-devtestlab/master/Samples/201-dtl-create-customimage-from-vhd/azuredeploy.json' -TemplateParameterObject $parameters
