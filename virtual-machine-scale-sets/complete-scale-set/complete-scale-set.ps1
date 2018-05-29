869cbb31768bbb44317874d108e0e8eac5bac27b
Iain Foulds <iain.foulds@microsoft.com>
2018-05-29 16:05:11 -0700

    [VMSS] Long-format example to create a scale set (#129)

---
+++ virtual-machine-scale-sets/complete-scale-set/complete-scale-set.ps1
@@ -0,0 +1,121 @@
+# Provide your own secure password for use with the VM instances
+$cred = Get-Credential
+
+# Define variables for resource names
+$resourceGroupName = "myResourceGroup"
+$scaleSetName = "myScaleSet"
+$location = "EastUS"
+
+# Create a resource group
+New-AzureRmResourceGroup -ResourceGroupName $resourceGroupName -Location $location
+
+# Create a virtual network subnet
+$subnet = New-AzureRmVirtualNetworkSubnetConfig `
+  -Name "mySubnet" `
+  -AddressPrefix 10.0.0.0/24
+
+# Create a virtual network
+$vnet = New-AzureRmVirtualNetwork `
+  -ResourceGroupName $resourceGroupName `
+  -Name "myVnet" `
+  -Location $location `
+  -AddressPrefix 10.0.0.0/16 `
+  -Subnet $subnet
+
+# Create a public IP address
+$publicIP = New-AzureRmPublicIpAddress `
+  -ResourceGroupName $resourceGroupName `
+  -Location $location `
+  -AllocationMethod Static `
+  -Name "myPublicIP"
+
+# Create a frontend and backend IP pool
+$frontendIP = New-AzureRmLoadBalancerFrontendIpConfig `
+  -Name "myFrontEndPool" `
+  -PublicIpAddress $publicIP
+$backendPool = New-AzureRmLoadBalancerBackendAddressPoolConfig -Name "myBackEndPool"
+
+# Create a Network Address Translation (NAT) pool
+# A rule for Remote Desktop Protocol (RDP) traffic is created on TCP port 3389
+$inboundNATPool = New-AzureRmLoadBalancerInboundNatPoolConfig `
+  -Name "myRDPRule" `
+  -FrontendIpConfigurationId $frontendIP.Id `
+  -Protocol TCP `
+  -FrontendPortRangeStart 50001 `
+  -FrontendPortRangeEnd 50010 `
+  -BackendPort 3389
+
+# Create the load balancer
+$lb = New-AzureRmLoadBalancer `
+  -ResourceGroupName $resourceGroupName `
+  -Name "myLoadBalancer" `
+  -Location $location `
+  -FrontendIpConfiguration $frontendIP `
+  -BackendAddressPool $backendPool `
+  -InboundNatPool $inboundNATPool
+
+# Create a load balancer health probe for TCP port 80
+Add-AzureRmLoadBalancerProbeConfig -Name "myHealthProbe" `
+  -LoadBalancer $lb `
+  -Protocol TCP `
+  -Port 80 `
+  -IntervalInSeconds 15 `
+  -ProbeCount 2
+
+# Create a load balancer rule to distribute traffic on port TCP 80
+# The health probe from the previous step is used to make sure that traffic is
+# only directed to healthy VM instances
+Add-AzureRmLoadBalancerRuleConfig `
+  -Name "myLoadBalancerRule" `
+  -LoadBalancer $lb `
+  -FrontendIpConfiguration $lb.FrontendIpConfigurations[0] `
+  -BackendAddressPool $lb.BackendAddressPools[0] `
+  -Protocol TCP `
+  -FrontendPort 80 `
+  -BackendPort 80 `
+  -Probe (Get-AzureRmLoadBalancerProbeConfig -Name "myHealthProbe" -LoadBalancer $lb)
+
+# Update the load balancer configuration
+Set-AzureRmLoadBalancer -LoadBalancer $lb
+
+# Create IP address configurations
+$ipConfig = New-AzureRmVmssIpConfig `
+  -Name "myIPConfig" `
+  -LoadBalancerBackendAddressPoolsId $lb.BackendAddressPools[0].Id `
+  -LoadBalancerInboundNatPoolsId $inboundNATPool.Id `
+  -SubnetId $vnet.Subnets[0].Id
+
+# Create a config object
+# The VMSS config object stores the core information for creating a scale set
+$vmssConfig = New-AzureRmVmssConfig `
+    -Location $location `
+    -SkuCapacity 2 `
+    -SkuName "Standard_DS2" `
+    -UpgradePolicyMode "Automatic"
+
+# Reference a virtual machine image from the gallery
+Set-AzureRmVmssStorageProfile $vmssConfig `
+  -OsDiskCreateOption "FromImage" `
+  -ImageReferencePublisher "MicrosoftWindowsServer" `
+  -ImageReferenceOffer "WindowsServer" `
+  -ImageReferenceSku "2016-Datacenter" `
+  -ImageReferenceVersion "latest"
+
+# Set up information for authenticating with the virtual machine
+Set-AzureRmVmssOsProfile $vmssConfig `
+  -AdminUsername $cred.UserName `
+  -AdminPassword $cred.Password `
+  -ComputerNamePrefix "myVM"
+
+# Attach the virtual network to the config object
+Add-AzureRmVmssNetworkInterfaceConfiguration `
+  -VirtualMachineScaleSet $vmssConfig `
+  -Name "network-config" `
+  -Primary $true `
+  -IPConfiguration $ipConfig
+
+# Create the scale set with the config object (this step might take a few minutes)
+New-AzureRmVmss `
+  -ResourceGroupName $resourceGroupName `
+  -Name $scaleSetName `
+  -VirtualMachineScaleSet $vmssConfig
