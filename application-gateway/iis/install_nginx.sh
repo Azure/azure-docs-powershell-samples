4808e9a712987d57829ac0956cb797758da237c8
Vic <VictorH@microsoft.com>
2018-07-13 13:34:28 -0700

    Move script from davidmu1

---
+++ application-gateway/iis/install_nginx.sh
@@ -0,0 +1,6 @@
+apt-get install -y nginx
+echo $(hostname) | sudo tee /var/www/html/index.html
+sudo mkdir -p /var/www/html/images
+echo "Images: " $(hostname) | sudo tee /var/www/html/images/test.html
+sudo mkdir -p /var/www/html/video
+echo "Video: " $(hostname) | sudo tee /var/www/html/video/test.html
