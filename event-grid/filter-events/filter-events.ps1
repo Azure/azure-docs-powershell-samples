e74b00c6459a84df10f46a5687fc20ddf2181a57
Tom FitzMacken <tomfitz@microsoft.com>
2018-11-20 12:42:00 -0800

    fixes

--- event-grid/filter-events/filter-events.ps1
+++ event-grid/filter-events/filter-events.ps1
@@ -1,18 +1,21 @@
 # Provide an endpoint for handling the events. Must be formatted "https://your-endpoint-URL"
-$myEndpoint = "<endpoint URL>"
+$myEndpoint = "<your-endpoint-URL>"
 
 # Provide the name of the resource group to create. It will contain the network security group.
 # You will subscribe to events for this resource group.
-$myResourceGroup = "<resource-group>"
+$myResourceGroup = "<resource-group-name>"
+
+# Provide a name for the network security group to create.
+$nsgName = "<your-nsg-name>"
 
 # Create the resource group
 New-AzureRmResourceGroup -Name $myResourceGroup -Location westus2
 
 # Create a network security group. You will filter events to only those that are related to this resource.
-New-AzureRmNetworkSecurityGroup -Name "nsg1" -ResourceGroupName $myResourceGroup  -Location westus2
+New-AzureRmNetworkSecurityGroup -Name $nsgName -ResourceGroupName $myResourceGroup  -Location westus2
 
 # Get the resource ID to filter events
-$resourceId = (Get-AzureRmResource -ResourceName "nsg1" -ResourceGroupName $myResourceGroup).ResourceId
+$resourceId = (Get-AzureRmResource -ResourceName $nsgName -ResourceGroupName $myResourceGroup).ResourceId
 
 # Subscribe to the resource group. Provide the name of the resource group you want to subscribe to.
 New-AzureRmEventGridSubscription `
