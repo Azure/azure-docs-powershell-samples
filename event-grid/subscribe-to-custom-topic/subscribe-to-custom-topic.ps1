5d651d19a1a4681306b9a31fb31d69baac6e36ad
Tom FitzMacken <tomfitz@microsoft.com>
2018-11-20 12:55:09 -0800

    fixes

--- event-grid/subscribe-to-custom-topic/subscribe-to-custom-topic.ps1
+++ event-grid/subscribe-to-custom-topic/subscribe-to-custom-topic.ps1
@@ -1,11 +1,17 @@
 # Provide the name of the topic you are subscribing to
-$myTopic = "demoContosoTopic"
+$myTopic = "<your-custom-topic-name>"
 
-# Provide an endpoint for handling the events.
-$myEndpoint = "<endpoint URL>"
+# Provide an endpoint for handling the events. Must be formatted "https://your-endpoint-URL"
+$myEndpoint = "<your-endpoint-URL>"
 
-# Provide the name of the resource group containing the custom topic.
-$myResourceGroup="demoResourceGroup"
+# Provide a name for resource group to create. It will contain the custom event.
+$myResourceGroup = "<resource-group-name>"
+
+# Create resource group
+New-AzureRmResourceGroup -Name $myResourceGroup -Location westus2
+
+# Create custom topic
+New-AzureRmEventGridTopic -ResourceGroupName $myResourceGroup -Name $myTopic -Location westus2 
 
 # Subscribe to the custom event. Include the resource group that contains the custom topic.
 New-AzureRmEventGridSubscription `
