dd8e12b8bb7126beea2a69b6ee59c3ea68551aef
Tamra Myers <tamram@microsoft.com>
2019-01-24 09:27:23 -0800

    Update calculate-container-size.ps1

--- storage/calculate-container-size/calculate-container-size.ps1
+++ storage/calculate-container-size/calculate-container-size.ps1
@@ -1,10 +1,10 @@
 # this script will show how to get the total size of the blobs in a container
 # before running this, you need to create a storage account, create a container,
 #    and upload some blobs into the container 
-# note: this retrieves all of the blobs in the container in one command 
+# note: this retrieves all of the blobs in the container in one command. 
 #       if you are going to run this against a container with a lot of blobs
 #       (more than a couple hundred), use continuation tokens to retrieve
-#       the list of blobs. We will be adding a sample showing that scenario in the future.
+#       the list of blobs.
 
 # these are for the storage account to be used
 $resourceGroup = "bloblisttestrg"
