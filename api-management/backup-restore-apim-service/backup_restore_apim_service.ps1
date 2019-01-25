49944d89f49e8b3461061a94ac396deee811d4cb
Stephen Tramer <sttramer@microsoft.com>
2019-01-25 10:04:25 -0800

    Migrate api-management/ AzureRM->Az

--- api-management/backup-restore-apim-service/backup_restore_apim_service.ps1
+++ api-management/backup-restore-apim-service/backup_restore_apim_service.ps1
@@ -20,24 +20,24 @@ $containerName = "backups"
 $backupName = $apiManagementName + "-apimbackup"
  
 # Select default azure subscription
-Select-AzureRmSubscription -SubscriptionId $subscriptionId
+Select-AzSubscription -SubscriptionId $subscriptionId
  
 # Create a Resource Group 
-New-AzureRmResourceGroup -Name $resourceGroupName -Location $location -Force
+New-AzResourceGroup -Name $resourceGroupName -Location $location -Force
  
 # Create storage account    
-New-AzureRmStorageAccount -StorageAccountName $storageAccountName -Location $location -ResourceGroupName $resourceGroupName -Type Standard_LRS
-$storageKey = (Get-AzureRmStorageAccountKey -ResourceGroupName $resourceGroupName -StorageAccountName $storageAccountName)[0].Value
-$storageContext = New-AzureStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $storageKey
+New-AzStorageAccount -StorageAccountName $storageAccountName -Location $location -ResourceGroupName $resourceGroupName -Type Standard_LRS
+$storageKey = (Get-AzStorageAccountKey -ResourceGroupName $resourceGroupName -StorageAccountName $storageAccountName)[0].Value
+$storageContext = New-AzStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $storageKey
 
 # Create blob container
-New-AzureStorageContainer -Name $containerName -Context $storageContext -Permission blob
+New-AzStorageContainer -Name $containerName -Context $storageContext -Permission blob
  
 # Create API Management service
-New-AzureRmApiManagement -ResourceGroupName $resourceGroupName -Location $location -Name $apiManagementName -Organization $organisation -AdminEmail $adminEmail
+New-AzApiManagement -ResourceGroupName $resourceGroupName -Location $location -Name $apiManagementName -Organization $organisation -AdminEmail $adminEmail
  
 # Backup API Management service.
-Backup-AzureRmApiManagement -ResourceGroupName $resourceGroupName -Name $apiManagementName -StorageContext $storageContext -TargetContainerName $containerName -TargetBlobName $backupName
+Backup-AzApiManagement -ResourceGroupName $resourceGroupName -Name $apiManagementName -StorageContext $storageContext -TargetContainerName $containerName -TargetBlobName $backupName
  
 # Restore API Management service
-Restore-AzureRmApiManagement -ResourceGroupName $resourceGroupName -Name $apiManagementName -StorageContext $storageContext -SourceContainerName $containerName -SourceBlobName $backupName
+Restore-AzApiManagement -ResourceGroupName $resourceGroupName -Name $apiManagementName -StorageContext $storageContext -SourceContainerName $containerName -SourceBlobName $backupName
--- api-management/import-api-and-add-to-product/import_an_api_and_add_to_product.ps1
+++ api-management/import-api-and-add-to-product/import_an_api_and_add_to_product.ps1
@@ -20,26 +20,26 @@ $swaggerUrl = "http://petstore.swagger.io/v2/swagger.json"
 $apiPath = "petstore"
 
 # Set the context to the subscription Id where the cluster will be created
-Select-AzureRmSubscription -SubscriptionId $subscriptionId
+Select-AzSubscription -SubscriptionId $subscriptionId
 
 # Create a resource group.
-New-AzureRmResourceGroup -Name $resourceGroupName -Location $location
+New-AzResourceGroup -Name $resourceGroupName -Location $location
 
 # Create the Api Management service. Since the SKU is not specified, it creates a service with Developer SKU. 
-New-AzureRmApiManagement -ResourceGroupName $resourceGroupName -Name $apimServiceName -Location $location -Organization $organisation -AdminEmail $adminEmail
+New-AzApiManagement -ResourceGroupName $resourceGroupName -Name $apimServiceName -Location $location -Organization $organisation -AdminEmail $adminEmail
 
 # Create the API Management context
-$context = New-AzureRmApiManagementContext -ResourceGroupName $resourceGroupName -ServiceName $apimServiceName
+$context = New-AzApiManagementContext -ResourceGroupName $resourceGroupName -ServiceName $apimServiceName
 
 # import api from Url
-$api = Import-AzureRmApiManagementApi -Context $context -SpecificationUrl $swaggerUrl -SpecificationFormat Swagger -Path $apiPath
+$api = Import-AzApiManagementApi -Context $context -SpecificationUrl $swaggerUrl -SpecificationFormat Swagger -Path $apiPath
 
 $productName = "Pet Store Product"
 $productDescription = "Product giving access to Petstore api"
 $productState = "Published"
 
 # Create a Product to publish the Imported Api. This creates a product with a limit of 10 Subscriptions
-$product = New-AzureRmApiManagementProduct -Context $context -Title $productName -Description $productDescription -State $productState -SubscriptionsLimit 10 
+$product = New-AzApiManagementProduct -Context $context -Title $productName -Description $productDescription -State $productState -SubscriptionsLimit 10 
 
 # Add the petstore api to the published Product, so that it can be called in developer portal console
-Add-AzureRmApiManagementApiToProduct -Context $context -ProductId $product.ProductId -ApiId $api.ApiId
+Add-AzApiManagementApiToProduct -Context $context -ProductId $product.ProductId -ApiId $api.ApiId
