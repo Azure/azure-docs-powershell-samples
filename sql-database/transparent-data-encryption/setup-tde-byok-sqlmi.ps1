ef6663f5e091b2fec9b11d7bbcf81a0b2983c8b4
Van To (SQL) <vanto@microsoft.com>
2019-04-03 16:58:56 -0700

    Changes to TDE ps1

--- sql-database/transparent-data-encryption/setup-tde-byok-sqlmi.ps1
+++ sql-database/transparent-data-encryption/setup-tde-byok-sqlmi.ps1
@@ -4,8 +4,8 @@ Connect-AzAccount
 # If there are multiple subscriptions, choose the one where AKV is created: 
 Set-AzContext -SubscriptionId "subscription ID"
 
-# Install the preview version of AzureRM.Sql PowerShell package 4.11.6-preview if you are running this PowerShell locally (uncomment below):
-# Install-Module -Name AzureRM.Sql -RequiredVersion 4.11.6-preview -AllowPrerelease
+# Install the preview version of Az.Sql PowerShell package 1.1.1-preview if you are running this PowerShell locally (uncomment below):
+# Install-Module -Name Az.Sql -RequiredVersion 1.1.1-preview -AllowPrerelease -Force
 
 # 1. Create Resource and setup Azure Key Vault (skip if already done)
 
@@ -16,11 +16,9 @@ New-AzResourceGroup -Name "MyRG" -Location $location
 # Create new Azure Key Vault with soft-delete option turned on (change name, RG and region): 
 New-AzKeyVault -VaultName "MyKeyVault" -ResourceGroupName "MyRG" -Location $location -EnableSoftDelete
 
-# Create Managed Service Identity for the Managed Instance: 
-$instance = Set-AzSqlManagedInstance -ResourceGroupName "MyRG" -Name "MyManagedInstance" -AssignIdentity
-
 # Authorize Managed Instance to use the AKV (wrap/unwrap key and get public part of key, if public part exists): 
-Set-AzKeyVaultAccessPolicy -VaultName "MyKeyVault" -ServicePrincipalName $instance.Identity -PermissionsToKeys get,wrapKey,unwrapKey
+$objectid = (Get-AzResource -ResourceGroupName "MyRG" -Name "MyManagedInstance").Identity.PrincipalId
+Set-AzKeyVaultAccessPolicy -BypassObjectIdValidation -VaultName "MyKeyVault" -ObjectId $objectid -PermissionsToKeys get,wrapKey,unwrapKey
 
 # Allow access from trusted Azure services: 
 Update-AzKeyVaultNetworkRuleSet -VaultName "MyKeyVault" -Bypass AzureServices
@@ -35,15 +33,17 @@ Update-AzKeyVaultNetworkRuleSet -VaultName "MyKeyVault" -DefaultAction Deny
 # $key = Add-AzureKeyVaultKey -VaultName MyKeyVault -Name MyTDEKey -Destination Software -Size 2048
 
 # Alternatively, the recommended way is to import an existing key from a .pfx file:
-$securepfxpwd = ConvertTo-SecureString -String 'MyPa$$w0rd' -AsPlainText -Force 
-$key = Add-AzureKeyVaultKey -VaultName 'MyKeyVault' -Name 'MyTDEKey' -KeyFilePath 'c:\some_path\mytdekey.pfx' -KeyFilePassword $securepfxpwd
+$securepfxpwd = ConvertTo-SecureString -String "MyPa$$w0rd" -AsPlainText -Force 
+$key = Add-AzKeyVaultKey -VaultName "MyKeyVault" -Name "MyTDEKey" -KeyFilePath "c:\some_path\mytdekey.pfx" -KeyFilePassword $securepfxpwd
 
+# ...or get an existing key from the vault:
+# $key = Get-AzKeyVaultKey -VaultName "MyKeyVault" -Name "MyTDEKey"
 
 # 3. Set up BYOK TDE on Managed Instance:
 
 # Assign the key to the Managed Instance:
 # $key = 'https://contoso.vault.azure.net/keys/contosokey/01234567890123456789012345678901'
-Add-AzSqlManagedInstanceKeyVaultKey -KeyId $key -ManagedInstanceName "MyManagedInstance" -ResourceGroupName "MyRG"
+Add-AzSqlInstanceKeyVaultKey -KeyId $key.id -InstanceName "MyManagedInstance" -ResourceGroupName "MyRG"
 
 # Set TDE operation mode to BYOK: 
-Set-AzSqlManagedInstanceTransparentDataEncryptionProtector -Type AzureKeyVault -ManagedInstanceName "MyManagedInstance" -ResourceGroup "MyRG"
+Set-AzSqlInstanceTransparentDataEncryptionProtector -Type AzureKeyVault -InstanceName "MyManagedInstance" -ResourceGroup "MyRG" -KeyId $key.id
