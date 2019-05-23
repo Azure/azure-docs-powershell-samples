cf5cf6a3c23089687064da0cf502676f068afe65
Raman Kumar <ramankumarlive@users.noreply.github.com>
2019-05-23 15:01:40 -0700

    added instructions for AzCopy
    added instructions for AzCopy

--- virtual-machine/copy-managed-disks-vhd-to-storage-account/copy-managed-disks-vhd-to-storage-account.ps1
+++ virtual-machine/copy-managed-disks-vhd-to-storage-account/copy-managed-disks-vhd-to-storage-account.ps1
@@ -23,15 +23,28 @@ $storageAccountKey = 'yourStorageAccountKey'
 #Provide the name of the destination VHD file to which the VHD of the managed disk will be copied.
 $destinationVHDFileName = "yourvhdfilename"
 
+#Set the value to 1 to use AzCopy tool to download the data. This is the recommended option for faster copy.
+#Download AzCopy v10 from the link here: https://docs.microsoft.com/en-us/azure/storage/common/storage-use-azcopy-v10
+#Ensure that AzCopy is downloaded in the same folder as this file
+#If you set the value to 0 then Start-AzStorageBlobCopy will be used. Azure storage will asynchronously copy the data. 
+$useAzCopy = 1 
 
 # Set the context to the subscription Id where managed disk is created
 Select-AzSubscription -SubscriptionId $SubscriptionId
 
 #Generate the SAS for the managed disk 
 $sas = Grant-AzDiskAccess -ResourceGroupName $ResourceGroupName -DiskName $diskName -DurationInSecond $sasExpiryDuration -Access Read 
-Â 
-#Create the context of the storage account where the underlying VHD of the managed disk will be copied
-$destinationContext = New-AzStorageContext â€“StorageAccountName $storageAccountName -StorageAccountKey $storageAccountKeyÂ  
 
-#Copy the VHD of the managed disk to the storage account 
-Start-AzStorageBlobCopy -AbsoluteUri $sas.AccessSAS -DestContainer $storageContainerName -DestContext $destinationContext -DestBlob $destinationVHDFileName
+#Create the context of the storage account where the underlying VHD of the managed disk will be copied
+$destinationContext = New-AzStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $storageAccountKey
+
+#Copy the VHD of the managed disk to the storage account
+if($useAzCopy -eq 1)
+{
+    $containerSASURI = New-AzStorageContainerSASToken -Context $destinationContext -ExpiryTime(get-date).AddSeconds($sasExpiryDuration) -FullUri -Name $storageContainerName -Permission rw
+    .\azcopy copy $sas.AccessSAS $containerSASURI
+
+}else{
+
+    Start-AzStorageBlobCopy -AbsoluteUri $sas.AccessSAS -DestContainer $storageContainerName -DestContext $destinationContext -DestBlob $destinationVHDFileName
+}
