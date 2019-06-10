5cc0e5d95886b674737602797b043e469dcc71ab
Mark Brown <markjbrown@users.noreply.github.com>
2019-06-10 15:35:16 -0700

    fixed typo in failover priority script.

--- cosmosdb/common/ps-account-failover-priority-update.ps1
+++ cosmosdb/common/ps-account-failover-priority-update.ps1
@@ -1,15 +1,17 @@
 # Change the failover priority for an Azure Cosmos Account
 # Assume West US = 0 and East US = 1, the script below will flip them
 # Updating location with failoverPriority = 0 will trigger a failover
-$resourceGroupName = "myResourceGroup"
-$accountName = "mycosmosaccount"
 $resourceType = "Microsoft.DocumentDb/databaseAccounts"
 $apiVersion = "2015-04-08"
 
-$failoverPolicies = @(
+$failoverRegions = @(
     @{ "locationName"="East US"; "failoverPriority"=0 },
     @{ "locationName"="West US"; "failoverPriority"=1 }
 )
+
+$failoverPolicies = @{ 
+    "failoverPolicies"= $failoverRegions
+}
 
 Invoke-AzResourceAction -Action failoverPriorityChange `
     -ResourceType $resourceType -ApiVersion $apiVersion `
