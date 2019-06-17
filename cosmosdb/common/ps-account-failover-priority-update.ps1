d363910ca43f3f14268b6737b77fa64d3de6c2ed
Mark Brown <markjbrown@users.noreply.github.com>
2019-06-17 14:44:37 -0700

    fix missing variables

--- cosmosdb/common/ps-account-failover-priority-update.ps1
+++ cosmosdb/common/ps-account-failover-priority-update.ps1
@@ -1,6 +1,8 @@
 # Change the failover priority for an Azure Cosmos Account
 # Assume West US = 0 and East US = 1, the script below will flip them
 # Updating location with failoverPriority = 0 will trigger a failover
+$resourceGroupName = "myResourceGroup"
+$accountName = "mycosmosaccount"
 $resourceType = "Microsoft.DocumentDb/databaseAccounts"
 $apiVersion = "2015-04-08"
 
