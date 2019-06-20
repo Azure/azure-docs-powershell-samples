dec879553c405a74fa58bbdc15d31fb926a0c7ca
Mark Brown <markjbrown@users.noreply.github.com>
2019-06-20 07:30:47 -0700

    updating to include creating a new account

--- cosmosdb/common/ps-account-failover-priority-update.ps1
+++ cosmosdb/common/ps-account-failover-priority-update.ps1
@@ -1,10 +1,29 @@
 # Change the failover priority for a single-master Azure Cosmos Account
-# Assume West US 2 = 0 and East US 2 = 1, the script below will flip them
 # Note: Updating location with failoverPriority = 0 triggers a failover to the new region
+
+$location = "West US 2"
 $resourceGroupName = "myResourceGroup"
-$accountName = "mycosmosaccount"
+$accountName = "mycosmosaccount" # must be lower case.
 $resourceType = "Microsoft.DocumentDb/databaseAccounts"
 $apiVersion = "2015-04-08"
+
+# Provision a new Cosmos account with the regions below
+$locations = @(
+    @{ "locationName"="West US 2"; "failoverPriority"=0 },
+    @{ "locationName"="East US 2"; "failoverPriority"=1 }
+)
+
+$CosmosDBProperties = @{
+    "databaseAccountOfferType"="Standard";
+    "locations"=$locations
+}
+
+New-AzResource -ResourceType $resourceType `
+    -ApiVersion $apiVersion -ResourceGroupName $resourceGroupName -Location $location `
+    -Name $accountName -PropertyObject $CosmosDBProperties
+
+# Change the failover priority. Updating write region which will trigger a failover
+Read-Host -Prompt "Press any key to change the failover priority"
 
 $failoverRegions = @(
     @{ "locationName"="East US 2"; "failoverPriority"=0 },
