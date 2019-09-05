b27526d3a9b6871abcca4a0b91b33a013ab2f39e
cynthn <cynthn@windows.microsoft.com>
2019-09-05 16:20:25 -0700

    Fixing script

--- virtual-machine/create-managed-disk-from-snapshot/create-managed-disk-from-snapshot.ps1
+++ virtual-machine/create-managed-disk-from-snapshot/create-managed-disk-from-snapshot.ps1
@@ -27,6 +27,6 @@ Select-AzSubscription -SubscriptionId $SubscriptionId
 
 $snapshot = Get-AzSnapshot -ResourceGroupName $resourceGroupName -SnapshotName $snapshotName 
 
-$diskConfig = New-AzDiskConfig -AccountType $storageType -Location $location -CreateOption Copy -SourceResourceId $snapshot.Id
+$diskConfig = New-AzDiskConfig -SkuName $storageType -Location $location -CreateOption Copy -SourceResourceId $snapshot.Id
  
 New-AzDisk -Disk $diskConfig -ResourceGroupName $resourceGroupName -DiskName $diskName
