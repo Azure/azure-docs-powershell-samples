33ada99719448671ea96e8b68dc3ca1e0a265be1
MashaMSFT <mathoma@microsoft.com>
2019-09-26 14:41:45 -0700

    added clean up resource note

--- sql-database/failover-groups/add-managed-instance-to-failover-group-az-ps.ps1
+++ sql-database/failover-groups/add-managed-instance-to-failover-group-az-ps.ps1
@@ -642,4 +642,23 @@ Get-AzSqlDatabaseInstanceFailoverGroup -ResourceGroupName $resourceGroupName `
 
 
 # Clean up deployment 
-# Remove-AzResourceGroup -ResourceGroupName $resourceGroupName# Show randomized variables
+<# You will need to remove the resource group twice. Removing the resource group the first time will remove the managed instance and virtual clusters but will then fail with the error message `Remove-AzResourceGroup : Long running operation failed with status 'Conflict'.`. Run the Remove-AzResourceGroup command a second time to remove any residual resources as well as the resource group. #> 
+
+# Remove-AzResourceGroup -ResourceGroupName $resourceGroupName
+# Write-host "Removing managed instance and virtual cluster..."
+# Remove-AzResourceGroup -ResourceGroupName $resourceGroupName
+# Write-host "Removing residual resources and resouce group..."
+
+
+# Show randomized variables
+Write-host "Resource group name is" $resourceGroupName
+Write-host "Password is" $secpasswd
+Write-host "Primary Virtual Network name is" $primaryVNet
+Write-host "Primary default subnet name is" $primaryDefaultSubnet
+Write-host "Primary managed instance subnet name is" $primaryMiSubnetName
+Write-host "Secondary Virtual Network name is" $secondaryVNet
+Write-host "Secondary default subnet name is" $secondaryDefaultSubnet
+Write-host "Secondary managed instance subnet name is" $secondaryMiSubnetName
+Write-host "Primary managed instance name is" $primaryInstance
+Write-host "Secondary managed instance name is" $secondaryInstance
+Write-host "Failover group name is" $failoverGroupName
