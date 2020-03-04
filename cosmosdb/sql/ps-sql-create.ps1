3f3acce058434c7a13b5c81cfb740a7601efb416
Patrick El-Azem <paelaz@microsoft.com>
2020-03-04 15:05:06 -0500

    ps-sql-create.ps1: initial push for review

--- cosmosdb/sql/ps-sql-create.ps1
+++ cosmosdb/sql/ps-sql-create.ps1
@@ -1,86 +1,121 @@
-# Create a Cosmos SQL API account, single-master, a database with shared throughput, and a
-# container with its own dedicated thoughput, a unique key, and user defined conflict resolution path
+# --------------------------------------------------
+# References
+# Az.CosmosDB + cmdlets used below | https://docs.microsoft.com/powershell/module/az.cosmosdb
+# --------------------------------------------------
+# Purpose
+# Create a Cosmos SQL API account, a database with shared throughput, and two containers:
+# One container with shared throughput, and one container with its own dedicated thoughput,
+# custom indexing policy, a unique key, and user defined conflict resolution path
+# --------------------------------------------------
+Write-Host "Start:"(Get-Date -Format "O")
+# --------------------------------------------------
+# Functions / Utility
+Function New-RandomString{Param ([Int]$Length = 10) return $(-join ((97..122) + (48..57) | Get-Random -Count $Length | ForEach-Object {[char]$_}))}
 
-#generate a random 10 character alphanumeric string to ensure unique resource names
-$uniqueId=$(-join ((97..122) + (48..57) | Get-Random -Count 15 | % {[char]$_}))
+Function Get-VNetRuleId{Param ([String]$ResourceGroupName, [String]$VNetName, [String]$SubnetName)
+	$vnet = Get-AzResource -Name $VNetName -ResourceGroupName $ResourceGroupName -ResourceType "Microsoft.Network/virtualNetworks"
+	return ($vnet.ResourceId + "/subnets/" + $SubnetName)}
+# --------------------------------------------------
+$uniqueId = New-RandomString -Length 5		# Random alphanumeric string for unique resource names
+$apiKind = "GlobalDocumentDB"				# Required for SQL
+# --------------------------------------------------
+# Variables - ***** SUBSTITUTE YOUR VALUES *****
+$locations = @("East US")					# Regions ordered by failover priority; comma-separated
+$resourceGroupName = "cosmos"
 
-$apiVersion = "2015-04-08"
-$location = "West US 2"
-$resourceGroupName = "myResourceGroup"
-$accountName = "mycosmosaccount-$uniqueId" # must be lower case.
-$accountResourceType = "Microsoft.DocumentDb/databaseAccounts"
-$databaseResourceType = "Microsoft.DocumentDb/databaseAccounts/apis/databases"
-$containerResourceType = "Microsoft.DocumentDb/databaseAccounts/apis/databases/containers"
-$databaseName = "database1"
-$containerName = "container1"
-$databaseResourceName = $accountName + "/sql/" + $databaseName
-$containerResourceName = $accountName + "/sql/" + $databaseName + "/" + $containerName
-
-$locations = @(
-    @{ "locationName"="West US 2"; "failoverPriority"=0 },
-    @{ "locationName"="East US 2"; "failoverPriority"=1 }
-)
-
-$consistencyPolicy = @{
-    "defaultConsistencyLevel"="Session";
+$accountName = "cdb-sql-$uniqueId"			# Must be all lower case
+$consistencyLevel = "Session"
+$tags = @{
+	Department = "Snack Shack";
+	Purpose = "The Counting of Beans";
+	BillbackCode = 98765
 }
 
-$CosmosDBProperties = @{
-    "databaseAccountOfferType"="Standard";
-    "locations"=$locations;
-    "consistencyPolicy"=$consistencyPolicy;
-    "enableMultipleWriteLocations"="false"
-}
+$vnetName = "cosmos-vnet"					# Limit access to this VNet
+$subnetName = "subnet1"						# Limit access to this subnet in VNet
+$ignoreMissingVnetServiceEndpoint = $true	# Allow deployment if VNet does not have Cosmos DB service endpoint
 
-New-AzResource -ResourceType $accountResourceType -ApiVersion $apiVersion `
-    -ResourceGroupName $resourceGroupName -Location $location `
-    -Name $accountName -PropertyObject $CosmosDBProperties
+$ipRanges = @("75.68.47.183", "8.8.8.8")	# Firewall allowed IP addresses/ranges
 
+$databaseName = "db1"
+$databaseRUs = 1000
 
-#Database
-$databaseProperties = @{
-    "resource"=@{ "id"=$databaseName };
-    "options"=@{ "Throughput"= 400 }
-} 
-New-AzResource -ResourceType $databaseResourceType `
-    -ApiVersion $apiVersion -ResourceGroupName $resourceGroupName `
-    -Name $databaseResourceName -PropertyObject $databaseProperties
+$containerName1 = "shared_ru_container"
+$partitionKeyPath1 = "/myPartitionKey1"
 
+$containerName2 = "dedicated_ru_container"
+$containerRUs2 = 400
+$indexingPathIncluded2 = "/*"
+$indexingPathExcluded2 = "/myPathToNotIndex/*"
+$partitionKeyPath2 = "/myPartitionKey2"
+$uniqueKeyPath2 = "/myUniqueKeyPath"
+$conflictResolutionPath2 = "/myResolutionPath"
+$ttlInSeconds2 = 100						# Set this to -1 (or don't use) to never expire
 
-# Container
-$containerProperties = @{
-    "resource"=@{
-        "id"=$containerName; 
-        "partitionKey"=@{
-            "paths"=@("/myPartitionKey"); 
-            "kind"="Hash"
-        }; 
-        "indexingPolicy"=@{
-            "indexingMode"="Consistent"; 
-            "includedPaths"= @(@{
-                "path"="/*";
-            });
-            "excludedPaths"= @(@{
-                "path"="/myPathToNotIndex/*"
-            })
-        };
-        "uniqueKeyPolicy"= @{
-            "uniqueKeys"= @(@{
-                "paths"= @(
-                    "/myUniqueKey1";
-                    "/myUniqueKey2"
-                )
-            })
-        };
-        "defaultTtl"= 100;
-        "conflictResolutionPolicy"=@{
-            "mode"="lastWriterWins"; 
-            "conflictResolutionPath"="/myResolutionPath"
-        }
-    };
-    "options"=@{ "Throughput"= 400 }
-} 
+# --------------------------------------------------
+# VNet Access Restriction
+$vnetRuleId = Get-VNetRuleId -ResourceGroupName $resourceGroupName -VNetName $vnetName -SubnetName $subnetName
 
-New-AzResource -ResourceType $containerResourceType `
-    -ApiVersion $apiVersion -ResourceGroupName $resourceGroupName `
-    -Name $containerResourceName -PropertyObject $containerProperties
+$vnetRule1 = New-AzCosmosDBVirtualNetworkRule -Id $vnetRuleId -IgnoreMissingVNetServiceEndpoint $ignoreMissingVnetServiceEndpoint
+$vnetRules = @($vnetRule1)	# Array of VNet rules
+# --------------------------------------------------
+# Deploy Resources
+
+# Create Cosmos DB Account
+# TODO: when firewall is enabled, toggle grant access to Azure networks
+# TODO: when firewall is enabled, toggle portal access Data Explorer
+# TODO: when VNet access restriction is enabled, also enable external access in addition to above firewall TODOs
+#	-IpRangeFilter $ipRanges `
+#	-EnableVirtualNetwork -VirtualNetworkRuleObject $vnetRules -VirtualNetworkRule @($vnetRuleId) `
+$account = New-AzCosmosDBAccount `
+	-ResourceGroupName $resourceGroupName `
+	-Location $locations `
+	-Name $accountName `
+	-ApiKind $apiKind `
+	-DefaultConsistencyLevel $consistencyLevel `
+	-EnableAutomaticFailover `
+	-EnableMultipleWriteLocations `
+	-Tag $tags
+
+# Create Cosmos DB Database
+$database = Set-AzCosmosDBSqlDatabase `
+	-InputObject $account `
+	-Name $databaseName `
+	-Throughput $databaseRUs
+
+# Create Container with shared (from database) throughput
+Set-AzCosmosDBSqlContainer `
+	-InputObject $database `
+	-Name $containerName1 `
+	-PartitionKeyKind Hash `
+	-PartitionKeyPath $partitionKeyPath1
+
+# Create Container with dedicated throughput, indexing policy, unique key, conflict resolution policy
+$uniqueKey2 = New-AzCosmosDBSqlUniqueKey -Path $uniqueKeyPath2
+$uniqueKeyPolicy2 = New-AzCosmosDBSqlUniqueKeyPolicy -UniqueKey $uniqueKey2
+
+$includedPathIndex2 = New-AzCosmosDBSqlIncludedPathIndex -DataType String -Precision -1 -Kind Hash
+$includedPath2 = New-AzCosmosDBSqlIncludedPath -Path $indexingPathIncluded2 -Index $includedPathIndex2
+
+$indexingPolicy2 = New-AzCosmosDBSqlIndexingPolicy `
+	-IncludedPath $includedPath2 `
+	-ExcludedPath $indexingPathExcluded2 `
+	-IndexingMode Consistent `
+	-Automatic $true
+
+$conflictResolutionPolicy2 = New-AzCosmosDBSqlConflictResolutionPolicy `
+	-Type LastWriterWins `
+	-Path $conflictResolutionPath2
+
+Set-AzCosmosDBSqlContainer `
+	-InputObject $database `
+	-Name $containerName2 `
+	-IndexingPolicy $indexingPolicy2 `
+	-Throughput $containerRUs2 `
+	-PartitionKeyKind Hash `
+	-PartitionKeyPath $partitionKeyPath2 `
+	-UniqueKeyPolicy $uniqueKeyPolicy2 `
+	-ConflictResolutionPolicy $conflictResolutionPolicy2 `
+	-TtlInSeconds $ttlInSeconds2
+
+Write-Host "Complete:"(Get-Date -Format "O")
