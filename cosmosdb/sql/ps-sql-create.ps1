d50ae697b6ca6f1feb69fcf454f0ca4a5fb91b51
Patrick El-Azem <paelaz@microsoft.com>
2020-03-07 12:47:21 -0500

    Cosmos DB Powershell: ps-sql-create.ps1 rewrite for Az.CosmosDB cmdlets - final

--- cosmosdb/sql/ps-sql-create.ps1
+++ cosmosdb/sql/ps-sql-create.ps1
@@ -1,49 +1,42 @@
-# --------------------------------------------------
-# References
-# Az.CosmosDB + cmdlets used below | https://docs.microsoft.com/powershell/module/az.cosmosdb
+# Reference: Az.CosmosDB | https://docs.microsoft.com/powershell/module/az.cosmosdb
 # --------------------------------------------------
 # Purpose
-# Create a Cosmos SQL API account, a database with shared throughput, and a container with
-# dedicated thoughput, indexing policy, a unique key, and user defined conflict resolution path
-# ***** NOTE: this script can take several minutes to run to completion.
+# Create Cosmos SQL API account, database, and container with
+# dedicated thoughput, indexing policy, unique key, and conflict resolution
 # --------------------------------------------------
 Function New-RandomString{Param ([Int]$Length = 10) return $(-join ((97..122) + (48..57) | Get-Random -Count $Length | ForEach-Object {[char]$_}))}
 # --------------------------------------------------
-$uniqueId = New-RandomString -Length 4 # Random alphanumeric string for unique resource names
+$uniqueId = New-RandomString -Length 7 # Random alphanumeric string for unique resource names
 $apiKind = "GlobalDocumentDB"
 # --------------------------------------------------
 # Variables - ***** SUBSTITUTE YOUR VALUES *****
 $locations = @("East US", "West US") # Regions ordered by failover priority
-$resourceGroupName = "cosmos" # Resource Group must already exist
+$resourceGroupName = "myResourceGroup" # Resource Group must already exist
 $accountName = "cdb-$uniqueId" # Must be all lower case
 $consistencyLevel = "Session"
-$tags = @{Tag1 = "Tag1Text"; Tag2 = "Tag2Text"; Tag3 = "Tag3Text"}
-$databaseName = "db1"
-$containerName = "container1"
+$tags = @{Tag1 = "MyTag1"; Tag2 = "MyTag2"; Tag3 = "MyTag3"}
+$databaseName = "MyDatabase"
+$containerName = "MyContainer"
 $containerRUs = 400
 $indexPathIncluded = "/*"
 $indexPathExcluded = "/myPathToNotIndex/*"
 $partitionKeyPath = "/myPartitionKey"
 $uniqueKeyPath = "/myUniqueKeyPath"
 $conflictResolutionPath = "/myResolutionPath"
-$ttlInSeconds = -1 # Set this to -1 (or don't use it at all) to never expire
+$ttlInSeconds = 120 # Set this to -1 (or don't use it at all) to never expire
 # --------------------------------------------------
-# Cosmos DB Account
-$account = New-AzCosmosDBAccount `
-	-ResourceGroupName $resourceGroupName `
-	-Location $locations `
-	-Name $accountName `
-	-ApiKind $apiKind `
-	-DefaultConsistencyLevel $consistencyLevel `
-	-EnableAutomaticFailover `
-	-Tag $tags
+# Account
+$account = New-AzCosmosDBAccount -ResourceGroupName $resourceGroupName `
+	-Location $locations -Name $accountName -ApiKind $apiKind -Tag $tags `
+	-DefaultConsistencyLevel $consistencyLevel -EnableAutomaticFailover
 
-# Cosmos DB Database
-$database = Set-AzCosmosDBSqlDatabase `
-	-InputObject $account `
-	-Name $databaseName
+# Database
+$database = Set-AzCosmosDBSqlDatabase -InputObject $account -Name $databaseName
 
-# Container with dedicated throughput, unique key, indexing policy, conflict resolution policy
+# Container
+# Throughput should be 400 <= $containerRUs <= 100000 for dedicated
+if (($containerRUs -lt 400) -or ($containerRUs -gt 100000)) { $containerRUs = 400 }
+
 $uniqueKey = New-AzCosmosDBSqlUniqueKey -Path $uniqueKeyPath
 $uniqueKeyPolicy = New-AzCosmosDBSqlUniqueKeyPolicy -UniqueKey $uniqueKey
 
@@ -51,22 +44,16 @@ $includedPathIndex = New-AzCosmosDBSqlIncludedPathIndex -DataType String -Precis
 $includedPath = New-AzCosmosDBSqlIncludedPath -Path $indexPathIncluded -Index $includedPathIndex
 
 $indexingPolicy = New-AzCosmosDBSqlIndexingPolicy `
-	-IncludedPath $includedPath `
-	-ExcludedPath $indexPathExcluded `
-	-IndexingMode Consistent `
-	-Automatic $true
+	-IncludedPath $includedPath -ExcludedPath $indexPathExcluded `
+	-IndexingMode Consistent -Automatic $true
 
 $conflictResolutionPolicy = New-AzCosmosDBSqlConflictResolutionPolicy `
-	-Type LastWriterWins `
-	-Path $conflictResolutionPath
+	-Type LastWriterWins -Path $conflictResolutionPath
 
-Set-AzCosmosDBSqlContainer `
-	-InputObject $database `
-	-Name $containerName `
-	-IndexingPolicy $indexingPolicy `
-	-Throughput $containerRUs `
-	-PartitionKeyKind Hash `
-	-PartitionKeyPath $partitionKeyPath `
+$container = Set-AzCosmosDBSqlContainer `
+	-InputObject $database -Name $containerName `
+	-Throughput $containerRUs -IndexingPolicy $indexingPolicy `
+	-PartitionKeyKind Hash -PartitionKeyPath $partitionKeyPath `
 	-UniqueKeyPolicy $uniqueKeyPolicy `
 	-ConflictResolutionPolicy $conflictResolutionPolicy `
 	-TtlInSeconds $ttlInSeconds
