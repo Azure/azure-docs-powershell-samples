97633d45b223d8f61fd76b267103e75c5c2161e3
Patrick El-Azem <paelaz@microsoft.com>
2020-03-13 10:11:32 -0400

    Cosmos DB | Update ps-account-update.ps1 to use Set-AzResource pending Update-AzCosmosDBAccountRegion fix

--- cosmosdb/common/ps-account-update.ps1
+++ cosmosdb/common/ps-account-update.ps1
@@ -1,7 +1,7 @@
 # Reference: Az.CosmosDB | https://docs.microsoft.com/powershell/module/az.cosmosdb
 # --------------------------------------------------
 # Purpose
-# Update Cosmos SQL API account: Add an Azure region (location)
+# Update Cosmos DB account: Add an Azure region (location)
 # --------------------------------------------------
 # Variables - ***** SUBSTITUTE YOUR VALUES *****
 $resourceGroupName = "cosmos" # Resource Group must already exist
@@ -10,25 +10,29 @@ $locations = @("East US", "West US") # Regions ordered by failover priority
 # --------------------------------------------------
 
 # Get existing Cosmos DB account
-$account = Get-AzCosmosDBAccount -ResourceGroupName $resourceGroupName -Name $accountName
+# $account = Get-AzCosmosDBAccount -ResourceGroupName $resourceGroupName -Name $accountName
 
 # Fails with null ref exception as source requires both -Location and -LocationObject to be passed
-Update-AzCosmosDBAccountRegion -InputObject $account -Location $locations
-
-## TODO below is to try to get around issue where Update-AzCosmosDBAccountRegion source requires
-# both -Location and -LocationObject. Have not been able to get past eventual error 
-# List of supplied locations is invalid ActivityId: [GUID]], Microsoft.Azure.Documents.Common/2.10.0
-# When Update-AzCosmosDBAccountRegion accepts EITHER parameter without requiring BOTH
-# will update this example to use the above approach passing -Location.
-
+# and there is a separate issue with -LocationObject
 # Create location object array
+# $locationObjects = @()
+# $i = 0
+
+# Update-AzCosmosDBAccountRegion -InputObject $account -Location $locations
+# ForEach ($name in $locations){ $locationObjects += New-AzCosmosDBLocationObject -LocationName $name -FailoverPriority ($i++) }
+# Update-AzCosmosDBAccountRegion -InputObject $account -LocationObject $locationObjects
+
+# The following is to use Set-AzResource pending Update-AzCosmosDBAccountRegion fix
+$resourceType = "Microsoft.DocumentDb/databaseAccounts"
+$apiVersion = "2019-12-12"
 $locationObjects = @()
 $i = 0
-
-ForEach ($name in $locations){
-    $locationObjects += New-AzCosmosDBLocationObject -LocationName $name -FailoverPriority ($i++)
+ForEach ($location in $locations) { $locationObjects += @{ locationName = "$location"; failoverPriority = $i++ } }
+$CosmosDBProperties = @{
+    databaseAccountOfferType = "Standard";
+    locations = $locationObjects;
 }
 
-# Fails - whether location display names or names are used; whether AZ is specified or not,
-# whether I pass different regions for -Location and -LcoationObject etc.
-Update-AzCosmosDBAccountRegion -InputObject $account -Location $locations -LocationObject $locationObjects
+Set-AzResource -ResourceGroupName $resourceGroupName -ResourceType $resourceType `
+    -ApiVersion $apiVersion -Name $accountName `
+    -PropertyObject $CosmosDBProperties -Force
