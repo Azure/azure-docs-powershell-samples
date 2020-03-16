2f14c20bebc85f99628c07f40fea7fe529b44af4
Patrick El-Azem <paelaz@microsoft.com>
2020-03-16 06:38:10 -0400

    Cosmos DB | Common cleanup for PR

--- cosmosdb/common/ps-account-update.ps1
+++ cosmosdb/common/ps-account-update.ps1
@@ -12,17 +12,12 @@ $locations = @("East US", "West US") # Regions ordered by failover priority
 # Get existing Cosmos DB account
 # $account = Get-AzCosmosDBAccount -ResourceGroupName $resourceGroupName -Name $accountName
 
-# Fails with null ref exception as source requires both -Location and -LocationObject to be passed
-# and there is a separate issue with -LocationObject
-# Create location object array
-# $locationObjects = @()
-# $i = 0
-
+# Eventually transition to Update-AzCosmosDBAccountRegion with -Location or -LocationObject
 # Update-AzCosmosDBAccountRegion -InputObject $account -Location $locations
 # ForEach ($name in $locations){ $locationObjects += New-AzCosmosDBLocationObject -LocationName $name -FailoverPriority ($i++) }
 # Update-AzCosmosDBAccountRegion -InputObject $account -LocationObject $locationObjects
 
-# The following is to use Set-AzResource pending Update-AzCosmosDBAccountRegion fix
+# Use Set-AzResource with property object pending transition to Update-AzCosmosDBAccountRegion
 $resourceType = "Microsoft.DocumentDb/databaseAccounts"
 $apiVersion = "2019-12-12"
 $locationObjects = @()
