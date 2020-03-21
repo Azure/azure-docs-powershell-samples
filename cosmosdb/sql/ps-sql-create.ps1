417588ec86b3d41bff287c15383fca6a119bbbbb
Patrick El-Azem <paelaz@microsoft.com>
2020-03-21 08:39:41 -0400

    Cosmos DB | Add composite index to SQL create sample

--- cosmosdb/sql/ps-sql-create.ps1
+++ cosmosdb/sql/ps-sql-create.ps1
@@ -1,8 +1,8 @@
 # Reference: Az.CosmosDB | https://docs.microsoft.com/powershell/module/az.cosmosdb
 # --------------------------------------------------
 # Purpose
-# Create Cosmos SQL API account, database, and container with
-# dedicated thoughput, indexing policy, unique key, and conflict resolution
+# Create Cosmos SQL API account, database, and container with dedicated throughput,
+# indexing policy with include, exclude, and composite paths, unique key, and conflict resolution
 # --------------------------------------------------
 Function New-RandomString{Param ([Int]$Length = 10) return $(-join ((97..122) + (48..57) | Get-Random -Count $Length | ForEach-Object {[char]$_}))}
 # --------------------------------------------------
@@ -18,41 +18,63 @@ $tags = @{Tag1 = "MyTag1"; Tag2 = "MyTag2"; Tag3 = "MyTag3"}
 $databaseName = "myDatabase"
 $containerName = "myContainer"
 $containerRUs = 400
-$indexPathIncluded = "/*"
-$indexPathExcluded = "/myPathToNotIndex/*"
 $partitionKeyPath = "/myPartitionKey"
+$indexPathIncluded = "/*"
+$compositeIndexPaths1 = @(
+	@{ Path = "/myCompositePath1"; Order = "ascending" };
+	@{ Path = "/myCompositePath2"; Order = "descending" }
+)
+$compositeIndexPaths2 = @(
+	@{ Path = "/myCompositePath3"; Order = "ascending" };
+	@{ Path = "/myCompositePath4"; Order = "descending" }
+)
+$indexPathExcluded = "/myExcludedPath/*"
 $uniqueKeyPath = "/myUniqueKeyPath"
 $conflictResolutionPath = "/myResolutionPath"
 $ttlInSeconds = 120 # Set this to -1 (or don't use it at all) to never expire
 # --------------------------------------------------
-# Account
 Write-Host "Creating account $accountName"
 $account = New-AzCosmosDBAccount -ResourceGroupName $resourceGroupName `
 	-Location $locations -Name $accountName -ApiKind $apiKind -Tag $tags `
 	-DefaultConsistencyLevel $consistencyLevel `
 	-EnableAutomaticFailover:$true
 
-# Database
 Write-Host "Creating database $databaseName"
 $database = Set-AzCosmosDBSqlDatabase -InputObject $account -Name $databaseName
 
 $uniqueKey = New-AzCosmosDBSqlUniqueKey -Path $uniqueKeyPath
 $uniqueKeyPolicy = New-AzCosmosDBSqlUniqueKeyPolicy -UniqueKey $uniqueKey
 
+$compositePath1 = @()
+ForEach ($compositeIndexPath in $compositeIndexPaths1) {
+	$compositePath1 += New-AzCosmosDBSqlCompositePath `
+		-Path $compositeIndexPath.Path `
+		-Order $compositeIndexPath.Order
+}
+
+$compositePath2 = @()
+ForEach ($compositeIndexPath in $compositeIndexPaths2) {
+	$compositePath2 += New-AzCosmosDBSqlCompositePath `
+		-Path $compositeIndexPath.Path `
+		-Order $compositeIndexPath.Order
+}
+
 $includedPathIndex = New-AzCosmosDBSqlIncludedPathIndex -DataType String -Kind Range
 $includedPath = New-AzCosmosDBSqlIncludedPath -Path $indexPathIncluded -Index $includedPathIndex
 
 $indexingPolicy = New-AzCosmosDBSqlIndexingPolicy `
-	-IncludedPath $includedPath -ExcludedPath $indexPathExcluded `
+	-IncludedPath $includedPath `
+	-CompositePath @($compositePath1, $compositePath2) `
+	-ExcludedPath $indexPathExcluded `
 	-IndexingMode Consistent -Automatic $true
 
-# Conflict resolution policies only applicable in multi-master accounts.
-# Included here to show custom conflict resolution policy.
+# Conflict resolution policies only apply in multi-master accounts.
+# Included here to show custom resolution path.
 $conflictResolutionPolicy = New-AzCosmosDBSqlConflictResolutionPolicy `
 	-Type LastWriterWins -Path $conflictResolutionPath
 
 Write-Host "Creating container $containerName"
-$container = Set-AzCosmosDBSqlContainer `
+Set-AzCosmosDBSqlContainer `
 	-InputObject $database -Name $containerName `
 	-Throughput $containerRUs -IndexingPolicy $indexingPolicy `
 	-PartitionKeyKind Hash -PartitionKeyPath $partitionKeyPath `
