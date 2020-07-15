0a1995d4137b6269a0837e322fc25771af20e5b1
Prudence <43092500+prudencep@users.noreply.github.com>
2020-07-15 17:40:33 -0400

    Add files via upload

---
+++ maintenance-auto-scheduler/run.ps1
@@ -0,0 +1,28 @@
+# This Example is for a Dedicated Host
+
+# Input bindings are passed in via param block.
+param($Timer)
+
+# Check if any maintenance updates are available for your dedicated host
+$isMaintenance = Get-AzMaintenanceUpdate `
+    -ResourceGroupName test-scheduler `
+    -ResourceName windowscheduler-DHost `
+    -ResourceType hosts `
+    -ResourceParentName windowScheduler `
+    -ResourceParentType hostGroups `
+    -ProviderName Microsoft.Compute | Format-Table
+
+# if available, apply the update. Else, write that there are "no availabe updates" to the log
+if ($isMaintenance -ne $null)
+{
+New-AzApplyUpdate `
+    -ResourceGroupName test-scheduler `
+    -ResourceName windowscheduler-DHost `
+    -ResourceType hosts `
+    -ResourceParentName windowScheduler `
+    -ResourceParentType hostGroups `
+    -ProviderName Microsoft.Compute
+}
+else {
+Write-Host "No Updates Available"
+}
