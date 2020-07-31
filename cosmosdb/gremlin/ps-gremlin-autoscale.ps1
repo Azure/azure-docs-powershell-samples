77c32e3a6ccd06eef19b480ac3c2f735ee0f9a05
Mark Brown <markjbrown@users.noreply.github.com>
2020-07-30 18:25:43 -0700

    add autoscale, server version to mongodb (#285)

--- cosmosdb/gremlin/ps-gremlin-autoscale.ps1
+++ cosmosdb/gremlin/ps-gremlin-autoscale.ps1
@@ -1,44 +1,36 @@
 # Reference: Az.CosmosDB | https://docs.microsoft.com/powershell/module/az.cosmosdb
 # --------------------------------------------------
 # Purpose
-# Create Cosmos MongoDB API account with automatic failover,
-# a database, and a collection with dedicated throughput.
+# Create Cosmos Gremlin API account, database, and graph
+# with autoscale throughput
 # --------------------------------------------------
 Function New-RandomString{Param ([Int]$Length = 10) return $(-join ((97..122) + (48..57) | Get-Random -Count $Length | ForEach-Object {[char]$_}))}
 # --------------------------------------------------
 $uniqueId = New-RandomString -Length 7 # Random alphanumeric string for unique resource names
-$apiKind = "MongoDB"
+$apiKind = "Gremlin"
 # --------------------------------------------------
 # Variables - ***** SUBSTITUTE YOUR VALUES *****
 $locations = @("East US", "West US") # Regions ordered by failover priority
 $resourceGroupName = "myResourceGroup" # Resource Group must already exist
 $accountName = "cosmos-$uniqueId" # Must be all lower case
-$serverVersion = "3.6" #3.2 or 3.6
 $consistencyLevel = "Session"
-$tags = @{Tag1 = "MyTag1"; Tag2 = "MyTag2"; Tag3 = "MyTag3"}
 $databaseName = "myDatabase"
-$collectionName = "myCollection"
-$collectionRUs = 400
-$shardKey = "user_id"
-$partitionKeys = @("user_id", "user_address")
-$ttlKeys = @("_ts")
-$ttlInSeconds = 604800
+$graphName = "myGraph"
+$autoscaleMaxThroughput = 4000 #minimum = 4000
+$partitionKeys = @("/myPartitionKey")
 # --------------------------------------------------
+
 Write-Host "Creating account $accountName"
 $account = New-AzCosmosDBAccount -ResourceGroupName $resourceGroupName `
-    -Location $locations -Name $accountName -ApiKind $apiKind -Tag $tags `
+    -Location $locations -Name $accountName -ApiKind $apiKind `
     -DefaultConsistencyLevel $consistencyLevel `
-    -EnableAutomaticFailover:$true -MongoDBServerVersion $serverVersion
+    -EnableAutomaticFailover:$true
 
 Write-Host "Creating database $databaseName"
-$database = New-AzCosmosDBMongoDBDatabase -ParentObject $account `
+$database = New-AzCosmosDBGremlinDatabase -ParentObject $account `
     -Name $databaseName
 
-$index1 = New-AzCosmosDBMongoDBIndex -Key $partitionKeys -Unique $true
-$index2 = New-AzCosmosDBMongoDBIndex -Key $ttlKeys -TtlInSeconds $ttlInSeconds
-$indexes = @($index1, $index2)
-
-Write-Host "Creating collection $collectionName"
-$collection = New-AzCosmosDBMongoDBCollection -ParentObject $database `
-    -Name $collectionName -Throughput $collectionRUs `
-    -Shard $shardKey -Index $indexes
+Write-Host "Creating graph $graphName"
+$graph = New-AzCosmosDBGremlinGraph -ParentObject $database `
+    -Name $graphName -AutoscaleMaxThroughput $autoscaleMaxThroughput `
+    -PartitionKeyKind Hash -PartitionKeyPath $partitionKeys
