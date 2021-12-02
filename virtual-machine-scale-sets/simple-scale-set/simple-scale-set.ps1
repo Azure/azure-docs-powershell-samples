037bdcb25bf93428b9c9759505e5a9611a631df6
Mike F. Robbins <mikefrobbins@users.noreply.github.com>
2021-12-02 11:14:45 -0600

    Converted example to splatting per our contribution guide

--- virtual-machine-scale-sets/simple-scale-set/simple-scale-set.ps1
+++ virtual-machine-scale-sets/simple-scale-set/simple-scale-set.ps1
@@ -4,12 +4,14 @@ $cred = Get-Credential
 # Create a virtual machine scale set and supporting resources
 # A resource group, virtual network, load balancer, and NAT rules are automatically
 # created if they do not already exist
-New-AzVmss `
-  -ResourceGroupName "myResourceGroup" `
-  -VMScaleSetName "myScaleSet" `
-  -Location "EastUS" `
-  -VirtualNetworkName "myVnet" `
-  -SubnetName "mySubnet" `
-  -PublicIpAddressName "myPublicIPAddress" `
-  -LoadBalancerName "myLoadBalancer" `
-  -Credential $cred
+$Params = @{
+  ResourceGroupName = 'myResourceGroup'
+  VMScaleSetName = 'myScaleSet'
+  Location = 'EastUS'
+  VirtualNetworkName = 'myVnet'
+  SubnetName = 'mySubnet'
+  PublicIpAddressName = 'myPublicIPAddress'
+  LoadBalancerName = 'myLoadBalancer'
+  Credential = $cred
+}
+New-AzVmss @Params
