52035f666d21825af72c0741f24db8267f0a376c
Mike Urnun <36717703+mike-urnun-msft@users.noreply.github.com>
2023-04-12 18:19:00 -0700

    (Azure CXP) sample fix using Set-AzApiManagement
    resolves MicrosoftDocs/azure-docs#92378

--- api-management/setup-custom-domain/setup_custom_domain.ps1
+++ api-management/setup-custom-domain/setup_custom_domain.ps1
@@ -3,52 +3,35 @@
 #  of api management service.
 ###########################################################
 
-$random = (New-Guid).ToString().Substring(0,8)
-
-#Azure specific details
-$subscriptionId = "my-azure-subscription-id"
 
 # Api Management service specific details
-$apimServiceName = "apim-$random"
-$resourceGroupName = "apim-rg-$random"
-$location = "Japan East"
-$organisation = "Contoso"
-$adminEmail = "admin@contoso.com"
+$apimServiceName = "apim-service-name"
+$resourceGroupName = "apim-rg"
+$gatewayHostname = "api.contoso.com"                 # API gateway host
+$portalHostname = "portal.contoso.com"               # API developer portal host
 
-# Set the context to the subscription Id where the cluster will be created
-Select-AzSubscription -SubscriptionId $subscriptionId
+# Certificate specific details
+$gatewayCertCerPath = "C:\Users\Contoso\gateway.cer" # full path to api.contoso.net .cer file
+$gatewayCertPfxPath = "C:\Users\Contoso\gateway.pfx" # full path to api.contoso.net .pfx file
+$portalCertPfxPath = "C:\Users\Contoso\portal.pfx"   # full path to portal.contoso.net .pfx file
+$gatewayCertPfxPassword = "certificatePassword123"   # password for api.contoso.net pfx certificate
+$portalCertPfxPassword = "certificatePassword123"    # password for portal.contoso.net pfx certificate
 
-# Create a resource group.
-New-AzResourceGroup -Name $resourceGroupName -Location $location
-
-# Create the Api Management service. Since the SKU is not specified, it creates a service with Developer SKU. 
-New-AzApiManagement -ResourceGroupName $resourceGroupName -Name $apimServiceName -Location $location -Organization $organisation -AdminEmail $adminEmail
-
-# Certificate related details
-$proxyHostname = "proxy.contoso.net"
-# Certificate containing Common Name CN="proxy.contoso.net" or CN=*.contoso.net
-$proxyCertificatePath = "C:\proxycert.pfx"
-$proxyCertificatePassword = "certPassword"
-
-$portalHostname = "portal.contoso.net"
-# Certificate containing Common Name CN="portal.contoso.net" or CN=*.contoso.net
-$portalCertificatePath = "C:\portalcert.pfx"
-$portalCertificatePassword = "certPassword"
-
-# Upload the custom ssl certificate to be applied to Proxy endpoint / Api Gateway endpoint
-$proxyCertUploadResult = Import-AzApiManagementHostnameCertificate -Name $apimServiceName -ResourceGroupName $resourceGroupName `
-                        -HostnameType "Proxy" -PfxPath $proxyCertificatePath -PfxPassword $proxyCertificatePassword
-
-# Upload the custom ssl certificate to be applied to Portal endpoint
-$portalCertUploadResult = Import-AzApiManagementHostnameCertificate -Name $apimServiceName -ResourceGroupName $resourceGroupName `
-                        -HostnameType "Portal" -PfxPath $portalCertificatePath -PfxPassword $portalCertificatePassword
-
-# Create the HostnameConfiguration object for Portal endpoint
-$PortalHostnameConf = New-AzApiManagementHostnameConfiguration -Hostname $proxyHostname -CertificateThumbprint $proxyCertUploadResult.Thumbprint
+# Convert cert passwords into secure strings
+$gatewayCertPfxPasswordSecure = ConvertTo-SecureString -String $gatewayCertPfxPassword -AsPlainText -Force
+$portalCertPfxPasswordSecure = ConvertTo-SecureString -String $portalCertPfxPassword -AsPlainText -Force
 
 # Create the HostnameConfiguration object for Proxy endpoint
-$ProxyHostnameConf = New-AzApiManagementHostnameConfiguration -Hostname $portalHostname -CertificateThumbprint $portalCertUploadResult.Thumbprint
+$proxyHostnameConfig = New-AzApiManagementCustomHostnameConfiguration -Hostname $gatewayHostname -HostnameType Proxy -PfxPath $gatewayCertPfxPath -PfxPassword $gatewayCertPfxPasswordSecure
+# Create the HostnameConfiguration object for Portal endpoint
+$portalHostnameConfig = New-AzApiManagementCustomHostnameConfiguration -Hostname $portalHostname -HostnameType Portal -PfxPath $portalCertPfxPath -PfxPassword $portalCertPfxPasswordSecure
+
+# Get existing API Management instance object
+$apim = Get-AzApiManagement -ResourceGroupName $resourceGroupName -Name $apimServiceName
+
+# Set Proxy and Portal config objects
+$apim.ProxyCustomHostnameConfiguration = $proxyHostnameConfig
+$apim.PortalCustomHostnameConfiguration = $portalHostnameConfig
 
 # Apply the configuration to API Management
-Set-AzApiManagementHostnames -Name $apimServiceName -ResourceGroupName $resourceGroupName `
-        -PortalHostnameConfiguration $PortalHostnameConf -ProxyHostnameConfiguration $ProxyHostnameConf
+Set-AzApiManagement -InputObject $apim
