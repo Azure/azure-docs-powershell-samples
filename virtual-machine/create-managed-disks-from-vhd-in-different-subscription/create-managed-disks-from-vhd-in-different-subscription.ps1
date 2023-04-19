f531c31ea45bad4c1d00f07feccd09f565ea3abe
rogara <31490067+roygara@users.noreply.github.com>
2023-04-19 08:43:31 -0700

    Update create-managed-disks-from-vhd-in-different-subscription.ps1

--- virtual-machine/create-managed-disks-from-vhd-in-different-subscription/create-managed-disks-from-vhd-in-different-subscription.ps1
+++ virtual-machine/create-managed-disks-from-vhd-in-different-subscription/create-managed-disks-from-vhd-in-different-subscription.ps1
@@ -1,7 +1,26 @@
-#Provide the subscription Id where Managed Disks will be created
+
+<#
+
+.DESCRIPTION
+
+This sample demonstrates how to create a Managed Disk from a VHD file. 
+Create Managed Disks from VHD files in following scenarios:
+1. Create a Managed OS Disk from a specialized VHD file. A specialized VHD is a copy of VHD from an exisitng VM that maintains the user accounts, applications and other state data from your original VM. 
+   Attach this Managed Disk as OS disk to create a new virtual machine.
+2. Create a Managed data Disk from a VHD file. Attach the Managed Disk to an existing VM or attach it as data disk to create a new virtual machine.
+
+.NOTES
+
+1. Before you use this sample, please install the latest version of Azure PowerShell from here: http://go.microsoft.com/?linkid=9811175&clcid=0x409
+2. Provide the appropriate values for each variable. Note: The angled brackets should not be included in the values you provide.
+
+
+#>
+
+#Provide the subscription Id
 $subscriptionId = 'yourSubscriptionId'
 
-#Provide the name of your resource group where Managed Disks will be created. 
+#Provide the name of your resource group
 $resourceGroupName ='yourResourceGroupName'
 
 #Provide the name of the Managed Disk
@@ -10,28 +29,31 @@ $diskName = 'yourDiskName'
 #Provide the size of the disks in GB. It should be greater than the VHD file size.
 $diskSize = '128'
 
-#Provide the storage type for Managed Disk. Premium_LRS or Standard_LRS.
-$storageType = 'Premium_LRS'
+#Provide the URI of the VHD file that will be used to create Managed Disk. 
+# VHD file can be deleted as soon as Managed Disk is created.
+# e.g. https://contosostorageaccount1.blob.core.windows.net/vhds/contoso-um-vm120170302230408.vhd 
+$vhdUri = 'https://contosoststorageaccount1.blob.core.windows.net/vhds/contosovhd123.vhd' 
 
-#Provide the Azure region (e.g. westus) where Managed Disk will be located.
-#This location should be same as the storage account where VHD file is stored
+#Provide the storage type for the Managed Disk. PremiumLRS or StandardLRS.
+$sku = 'PremiumLRS'
+
+#Provide the Azure location (e.g. westus) where Managed Disk will be located. 
+#The location should be same as the location of the storage account where VHD file is stored.
 #Get all the Azure location using command below:
-#Get-AzLocation
+#Get-AzureRmLocation
 $location = 'westus'
 
-#Provide the URI of the VHD file (page blob) in a storage account. Please not that this is NOT the SAS URI of the storage container where VHD file is stored. 
-#e.g. https://contosostorageaccount1.blob.core.windows.net/vhds/contosovhd123.vhd
-#Note: VHD file can be deleted as soon as Managed Disk is created.
-$sourceVHDURI = 'https://contosostorageaccount1.blob.core.windows.net/vhds/contosovhd123.vhd'
-
-#Provide the resource Id of the storage account where VHD file is stored.
-#e.g. /subscriptions/6472s1g8-h217-446b-b509-314e17e1efb0/resourceGroups/MDDemo/providers/Microsoft.Storage/storageAccounts/contosostorageaccount
-#This is an optional parameter if you are creating managed disk in the same subscription
-$storageAccountId = '/subscriptions/yourSubscriptionId/resourceGroups/yourResourceGroupName/providers/Microsoft.Storage/storageAccounts/yourStorageAccountName'
-
 #Set the context to the subscription Id where Managed Disk will be created
-Select-AzSubscription -SubscriptionId $SubscriptionId
+Set-AzContext -Subscription $subscriptionId
 
-$diskConfig = New-AzDiskConfig -AccountType $storageType -Location $location -CreateOption Import -StorageAccountId $storageAccountId -SourceUri $sourceVHDURI -OsType $OsType -DiskSizeGB $diskSize
+#If you're creating an OS disk, add the following lines
+#Acceptable values are either Windows or Linux
+#$OSType = 'yourOSType'
+#Acceptable values are either V1 or V2
+#$HyperVGeneration = 'yourHyperVGen'
 
-New-AzDisk -Disk $diskConfig -ResourceGroupName $resourceGroupName -DiskName $diskName
+#If you're creating an OS disk, add -HyperVGeneration and -OSType parameters
+$diskConfig = New-AzDiskConfig -SkuName $sku -Location $location -DiskSizeGB $diskSize -SourceUri $vhdUri -CreateOption 'FromImage'
+
+#Create Managed disk
+New-AzDisk -DiskName $diskName -Disk $diskConfig -ResourceGroupName $resourceGroupName
