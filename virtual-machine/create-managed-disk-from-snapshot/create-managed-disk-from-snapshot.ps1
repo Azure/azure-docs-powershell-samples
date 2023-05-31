2dfaeafd476bb5fce8edaf8b698c178886a02ecd
rogara <31490067+roygara@users.noreply.github.com>
2023-05-31 16:20:28 -0700

    Updating to accommodate Premium SSD v2 and Ultra Disk

--- virtual-machine/create-managed-disk-from-snapshot/create-managed-disk-from-snapshot.ps1
+++ virtual-machine/create-managed-disk-from-snapshot/create-managed-disk-from-snapshot.ps1
@@ -13,8 +13,12 @@ $diskName = 'yourManagedDiskName'
 #Provide the size of the disks in GB. It should be greater than the VHD file size.
 $diskSize = '128'
 
-#Provide the storage type for Managed Disk. PremiumLRS or StandardLRS.
+#Provide the storage type for Managed Disk. Acceptable values are Standard_LRS, Premium_LRS, PremiumV2_LRS, StandardSSD_LRS, UltraSSD_LRS, Premium_ZRS and StandardSSD_ZRS.
 $storageType = 'Premium_LRS'
+
+#Required for Premium SSD v2 and Ultra Disks
+#Provide the Availability Zone you'd like the disk to be created in, default is 1
+$zone=1
 
 #Provide the Azure region (e.g. westus) where Managed Disks will be located.
 #This location should be same as the snapshot location
@@ -27,6 +31,7 @@ Select-AzSubscription -SubscriptionId $SubscriptionId
 
 $snapshot = Get-AzSnapshot -ResourceGroupName $resourceGroupName -SnapshotName $snapshotName 
 
+#If you're creating a Premium SSD v2 or an Ultra Disk, add "-Zone $zone" to the end of the command
 $diskConfig = New-AzDiskConfig -SkuName $storageType -Location $location -CreateOption Copy -SourceResourceId $snapshot.Id -DiskSizeGB $diskSize
  
 New-AzDisk -Disk $diskConfig -ResourceGroupName $resourceGroupName -DiskName $diskName
